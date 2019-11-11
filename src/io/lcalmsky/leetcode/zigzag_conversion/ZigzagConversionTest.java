9ce74cdc3109df88e3a074c6f129957976b0d8cd
Jungmin Lee <lcalmsky@gmail.com>
2019-11-11 21:13:18 +0900

    Solve the problem

---
+++ src/io/lcalmsky/leetcode/zigzag_conversion/ZigzagConversionTest.java
@@ -0,0 +1,31 @@
+package io.lcalmsky.leetcode.zigzag_conversion;
+
+public class ZigzagConversionTest {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+
+        StringBuilder sb = new StringBuilder();
+        int step = 2 * numRows - 2;
+        int k, step1, step2;
+        boolean flag;
+        for (int i = 0; i < numRows; i++) {
+            if (i == 0 || i == numRows - 1) {
+                for (int j = i; j < s.length(); j = j + step) {
+                    sb.append(s.charAt(j));
+                }
+            } else {
+                k = i;
+                flag = true;
+                step1 = 2 * (numRows - 1 - i);
+                step2 = step - step1;
+                while (k < s.length()) {
+                    sb.append(s.charAt(k));
+                    if (flag) k = k + step1;
+                    else k = k + step2;
+                    flag = !flag;
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
