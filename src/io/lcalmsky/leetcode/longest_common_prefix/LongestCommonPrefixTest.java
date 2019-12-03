4a422975aa4cb2db2cc4a66fa17922015b209a43
이정민(Jungmin Lee)/Payment개발팀/11ST <lcalmsky@gmail.com>
2019-12-03 13:36:32 +0900

    Solve the problem

--- src/io/lcalmsky/leetcode/longest_common_prefix/LongestCommonPrefixTest.java
+++ src/io/lcalmsky/leetcode/longest_common_prefix/LongestCommonPrefixTest.java
@@ -1,5 +1,8 @@
 package io.lcalmsky.leetcode.longest_common_prefix;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestCommonPrefixTest {
 
     public static void main(String[] args) {
@@ -9,20 +12,28 @@ public class LongestCommonPrefixTest {
     }
 
     public String longestCommonPrefix(String[] strs) {
-        if (strs.length < 2) return "";
+        if (strs.length == 0) return "";
+        if (strs.length < 2) return strs[0];
 
         StringBuilder sb = new StringBuilder();
 
-        int min = 0;
+        int min = Integer.MAX_VALUE;
         for (int i = 0; i < strs.length; i++) {
             if ((min = Math.min(strs[i].length(), min)) == 0) return "";
         }
 
-
-        for (int i = 0; i < min; i ++){
-            for (int j = 0; j < strs.length; j++){
-
+        Set<Character> set = new HashSet<>();
+        boolean flag = false;
+        char c = 0;
+        for (int i = 0; i < min; i++) {
+            set.add(strs[0].charAt(i));
+            for (int j = 1; j < strs.length; j++) {
+                c = strs[j].charAt(i);
+                if (flag = set.add(c)) break;
             }
+            if (!flag) sb.append(c);
+            else break;
+            set.clear();
         }
 
         return sb.toString();
