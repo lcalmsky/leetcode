1dc9ea9f99af43b74d4ede2157fdfe7ab33bdcce
이정민(Jungmin Lee)/Payment개발팀/11ST <lcalmsky@gmail.com>
2019-12-03 13:41:23 +0900

    Improve runtime and memory

--- src/io/lcalmsky/leetcode/longest_common_prefix/LongestCommonPrefixTest.java
+++ src/io/lcalmsky/leetcode/longest_common_prefix/LongestCommonPrefixTest.java
@@ -1,8 +1,5 @@
 package io.lcalmsky.leetcode.longest_common_prefix;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class LongestCommonPrefixTest {
 
     public static void main(String[] args) {
@@ -22,18 +19,15 @@ public class LongestCommonPrefixTest {
             if ((min = Math.min(strs[i].length(), min)) == 0) return "";
         }
 
-        Set<Character> set = new HashSet<>();
         boolean flag = false;
         char c = 0;
         for (int i = 0; i < min; i++) {
-            set.add(strs[0].charAt(i));
+            c = strs[0].charAt(i);
             for (int j = 1; j < strs.length; j++) {
-                c = strs[j].charAt(i);
-                if (flag = set.add(c)) break;
+                if (flag = !(c == strs[j].charAt(i))) break;
             }
             if (!flag) sb.append(c);
             else break;
-            set.clear();
         }
 
         return sb.toString();
