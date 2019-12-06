45457d2f4587cd87e4082cd65ec9eb71715b2cf5
이정민(Jungmin Lee)/Payment개발팀/11ST <lcalmsky@gmail.com>
2019-12-06 10:11:55 +0900

    Solve the problem using DFS (Depth-First Search)

--- src/io/lcalmsky/leetcode/generate_parentheses/GenerateParenthesesTests.java
+++ src/io/lcalmsky/leetcode/generate_parentheses/GenerateParenthesesTests.java
@@ -1,4 +1,29 @@
 package io.lcalmsky.leetcode.generate_parentheses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenerateParenthesesTests {
+    public static void main(String[] args) {
+        GenerateParenthesesTests generateParenthesesTests = new GenerateParenthesesTests();
+        System.out.println(generateParenthesesTests.generateParenthesis(1));
+        System.out.println(generateParenthesesTests.generateParenthesis(2));
+        System.out.println(generateParenthesesTests.generateParenthesis(3));
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        dfs(result, "", n, n);
+        return result;
+    }
+
+    private void dfs(List<String> result, String s, int left, int right) {
+        if (left > right) return;
+        if (left == 0 && right == 0) {
+            result.add(s);
+            return;
+        }
+        if (left > 0) dfs(result, s + "(", left - 1, right);
+        if (right > 0) dfs(result, s + ")", left, right - 1);
+    }
 }
