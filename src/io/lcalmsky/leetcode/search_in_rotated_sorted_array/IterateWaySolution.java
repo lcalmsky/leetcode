a936399521f800d141005e029cc5767e3a65aa7f
Jungmin Lee <lcalmsky@gmail.com>
2019-12-10 10:42:35 +0900

    Solve the problem
    - iterate way
    - Runtime: 0 ms (100%)
    - Memory: 39.7 MB (15.73%)

---
+++ src/io/lcalmsky/leetcode/search_in_rotated_sorted_array/IterateWaySolution.java
@@ -0,0 +1,38 @@
+package io.lcalmsky.leetcode.search_in_rotated_sorted_array;
+
+public class IterateWaySolution {
+    public static void main(String[] args) {
+        IterateWaySolution iterateWaySolution = new IterateWaySolution();
+        System.out.println(iterateWaySolution.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+        System.out.println(iterateWaySolution.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 3));
+        System.out.println(iterateWaySolution.search(new int[]{1, 3}, 1));
+        System.out.println(iterateWaySolution.search(new int[]{1, 3}, 2));
+    }
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target == nums[mid])
+                return mid;
+
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
