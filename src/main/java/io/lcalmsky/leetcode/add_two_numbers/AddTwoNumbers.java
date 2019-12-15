285816c15b3e1c30844ca7df666a6ce971c05470
Jungmin Lee <lcalmsky@gmail.com>
2019-12-16 08:37:56 +0900

    Solving problem
    - solve 5+5

--- src/main/java/io/lcalmsky/leetcode/add_two_numbers/AddTwoNumbers.java
+++ src/main/java/io/lcalmsky/leetcode/add_two_numbers/AddTwoNumbers.java
@@ -1,10 +1,5 @@
 package io.lcalmsky.leetcode.add_two_numbers;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -13,56 +8,23 @@ import java.util.stream.Collectors;
  * ListNode(int x) { val = x; }
  * }
  */
-class AddTwoNumbers {
+public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        List<Integer> list1 = new ArrayList<>();
-        while (l1.next != null) {
-            list1.add(l1.val);
-            l1 = l1.next;
-        }
-        List<Integer> list2 = new ArrayList<>();
-        while (l2.next != null) {
-            list2.add(l2.val);
-            l2 = l2.next;
-        }
-
-        Collections.reverse(list1);
-        Collections.reverse(list2);
-
-        int i1 = Integer.parseInt(list1.stream().map(String::valueOf).collect(Collectors.joining()));
-        int i2 = Integer.parseInt(list2.stream().map(String::valueOf).collect(Collectors.joining()));
-
-        int sum = i1 + i2;
-
-        ListNode result = null;
-        ListNode tmp1 = null;
-        ListNode tmp2 = null;
-
-        while (sum > 10) {
-            tmp1 = new ListNode(sum % 10);
-            if (tmp2 != null) tmp2.next = tmp1;
-            else tmp2 = tmp1;
-            sum /= 10;
+        ListNode p1 = l1, p2 = l2, head = null, prev = null, curr = null;
+        short c = 0;
+        while (p1 != null || p2 != null) {
+            c += p1 != null ? p1.val : 0;
+            c += p2 != null ? p2.val : 0;
+            curr = new ListNode(c % 10);
+            c /= 10;
+            if (head == null) head = curr;
+            if (prev != null) prev.next = curr;
+            prev = curr;
+            if (p1 != null) p1 = p1.next;
+            if (p2 != null) p2 = p2.next;
         }
 
-
-        return tmp2;
-    }
-
-    public static void main(String[] args) {
-
-
-        ListNode l1 = null; // 243
-        ListNode l2 = null; // 564
-        l1 = new ListNode(2);
-        l1.next = new ListNode(4);
-        l1.next.next = new ListNode(3);
-
-        l2 = new ListNode(5);
-        l2.next = new ListNode(6);
-        l2.next.next = new ListNode(4);
-
-        System.out.println(new AddTwoNumbers().addTwoNumbers(l1, l2));
+        return head;
     }
 }
 
--- src/test/java/io/lcalmsky/leetcode/add_two_numbers/AddTwoNumbersTests.java
+++ src/test/java/io/lcalmsky/leetcode/add_two_numbers/AddTwoNumbersTests.java
@@ -1,5 +1,6 @@
 package io.lcalmsky.leetcode.add_two_numbers;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -7,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddTwoNumbersTests {
     @Test
+    @DisplayName("243 + 564 = 708")
     public void givenTwoNumbers_whenAdd_thenCorrect() {
         // given
         AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
@@ -20,13 +22,34 @@ public class AddTwoNumbersTests {
         // when
         ListNode result = addTwoNumbers.addTwoNumbers(l1, l2);
 
-        System.out.println(result);
-
         // then
         assertAll(
                 () -> assertEquals(5, result.val),
                 () -> assertEquals(7, result.next.val),
                 () -> assertEquals(9, result.next.next.val)
         );
+
+        // log
+        System.out.println(result);
+    }
+
+    @Test
+    @DisplayName("5 + 5 = 10")
+    public void test2() {
+        // given
+        AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+        ListNode l1 = new ListNode(5);
+        ListNode l2 = new ListNode(5);
+
+        // when
+        ListNode result = addTwoNumbers.addTwoNumbers(l1, l2);
+
+        // then
+        assertAll(
+                () -> assertEquals(10, result.val)
+        );
+
+        // log
+        System.out.println(result);
     }
 }
