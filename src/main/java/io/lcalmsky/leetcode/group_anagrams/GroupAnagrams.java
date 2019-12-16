ca842bde301ddffffb7fb389a3dd21d0339d67f1
Jungmin Lee <lcalmsky@gmail.com>
2019-12-16 11:43:59 +0900

    Improve memory
    - Runtime: 8 ms (96.96%)
    - Memory: 41.7 MB (94.15%)

--- src/main/java/io/lcalmsky/leetcode/group_anagrams/GroupAnagrams.java
+++ src/main/java/io/lcalmsky/leetcode/group_anagrams/GroupAnagrams.java
@@ -1,19 +1,25 @@
 package io.lcalmsky.leetcode.group_anagrams;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class GroupAnagrams {
 
     public List<List<String>> groupAnagrams(String[] strs) {
 
         Map<String, List<String>> listMap = new LinkedHashMap<>();
-        char[] charArray;
+        char[] charArray, arr;
         List<String> list;
         String s;
         for (String str : strs) {
+            arr = new char[26];
             charArray = str.toCharArray();
-            Arrays.sort(charArray);
-            s = new String(charArray);
+            for (char c : charArray) {
+                arr[c - 'a']++;
+            }
+            s = new String(arr);
             if (!listMap.containsKey(s)) {
                 list = new ArrayList<>();
                 list.add(str);
