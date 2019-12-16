858039aa694e5763034a303abf51528dc28f2b1e
Jungmin Lee <lcalmsky@gmail.com>
2019-12-16 19:57:46 +0900

    Solve the problem
    - Runtime: 1 ms (98.82%)
    - Memory: 36 MB (100.00%)

---
+++ src/test/java/io/lcalmsky/leetcode/jump_game/JumpGameTests.java
@@ -0,0 +1,28 @@
+package io.lcalmsky.leetcode.jump_game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JumpGameTests {
+    @Test
+    public void givenArray_whenJump_thenCorrect() {
+        assertAll(
+                () -> test(new int[]{2, 3, 1, 1, 4}, true),
+                () -> test(new int[]{3, 2, 1, 0, 4}, false)
+        );
+    }
+
+    public void test(int[] given, boolean expected) {
+
+        // when
+        JumpGame jumpGame = new JumpGame();
+
+        boolean actual = jumpGame.canJump(given);
+
+        // then
+        assertEquals(actual, expected);
+
+    }
+}
