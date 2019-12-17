aa801399d057a14f141c60a4a60fc056a99408eb
Jungmin Lee <lcalmsky@gmail.com>
2019-12-17 09:52:06 +0900

    Improve solution
    - Runtime: 4 ms (16.16%)
    - Memory: 40.7 MB (90.63%)

--- src/main/java/io/lcalmsky/leetcode/insert_interval/InsertInterval.java
+++ src/main/java/io/lcalmsky/leetcode/insert_interval/InsertInterval.java
@@ -1,36 +1,27 @@
 package io.lcalmsky.leetcode.insert_interval;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class InsertInterval {
 
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        if (intervals == null || intervals.length == 0)
-            return newInterval == null || newInterval.length == 0
-                    ? new int[][]{}
-                    : new int[][]{newInterval};
-
-        int[][] array = new int[intervals.length + 1][2];
-        System.arraycopy(intervals, 0, array, 0, intervals.length);
-        array[intervals.length] = newInterval;
-
-        Arrays.sort(array, Comparator.comparingInt(o -> o[0]));
-
-        int[] interval = array[0];
         List<int[]> list = new ArrayList<>();
-        for (int[] ints : array) {
-            if (interval[1] >= ints[0]) {
-                interval[1] = Math.max(interval[1], ints[1]);
-            } else {
+
+        for (int[] interval : intervals) {
+            if (interval[1] < newInterval[0]) {
                 list.add(interval);
-                interval = ints;
+            } else if (interval[0] > newInterval[1]) {
+                list.add(newInterval);
+                newInterval = interval;
+            } else {
+                newInterval[0] = Math.min(interval[0], newInterval[0]);
+                newInterval[1] = Math.max(interval[1], newInterval[1]);
             }
         }
-        list.add(interval);
+
+        list.add(newInterval);
 
         int[][] result = new int[list.size()][2];
         IntStream.range(0, list.size()).forEach(i -> result[i] = list.get(i));
