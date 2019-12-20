2868039388bb281da9e88572dc1712f10027d1e5
Jungmin Lee <lcalmsky@gmail.com>
2019-12-20 14:42:28 +0900

    Solve the problem
    - Runtime: 0 ms (100.00%)
    - Memory Usage: 33.2 MB (5.10%)

---
+++ src/test/java/io/lcalmsky/leetcode/unique_paths/UniquePathsTests.java
@@ -0,0 +1,29 @@
+package io.lcalmsky.leetcode.unique_paths;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UniquePathsTests {
+
+    @Test
+    public void givenMatrixSize_whenCountUniquePaths_thenCorrect() {
+        assertAll(
+                () -> test(3, 2, 3),
+                () -> test(7, 3, 28)
+        );
+    }
+
+    public void test(int givenN, int givenM, int expected) {
+        // when
+        UniquePaths uniquePaths = new UniquePaths();
+        int actual = uniquePaths.uniquePaths(givenN, givenM);
+
+        // then
+        assertEquals(expected, actual);
+
+        // log
+        System.out.println(actual);
+    }
+}
