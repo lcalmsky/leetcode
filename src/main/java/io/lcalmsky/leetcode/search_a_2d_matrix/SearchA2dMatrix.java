26050d916b68bf4a4ed49fa1861535bbd7038742
Jungmin Lee <lcalmsky@gmail.com>
2019-12-23 12:12:28 +0900

    Solve the problem
    - my own solution
    - Runtime: 0 ms (100.00%)
    - Memory Usage: 41 MB (6.06%)

---
+++ src/main/java/io/lcalmsky/leetcode/search_a_2d_matrix/SearchA2dMatrix.java
@@ -0,0 +1,21 @@
+package io.lcalmsky.leetcode.search_a_2d_matrix;
+
+public class SearchA2dMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) return false;
+
+        if (target < matrix[0][0] || matrix[matrix.length - 1][matrix[matrix.length - 1].length - 1] < target)
+            return false;
+
+        int[] array;
+        for (int[] ints : matrix) {
+            array = ints;
+            if (array[0] <= target && array[array.length - 1] >= target) {
+                for (int value : array) if (value == target) return true;
+                return false;
+            }
+        }
+        return false;
+    }
+}
