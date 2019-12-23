515e166f26731488d2b2237e47261b78b5fd177c
Jungmin Lee <lcalmsky@gmail.com>
2019-12-23 09:28:40 +0900

    Divide solution and test

--- src/main/java/io/lcalmsky/leetcode/generate_parentheses/GenerateParentheses.java
+++ src/main/java/io/lcalmsky/leetcode/generate_parentheses/GenerateParentheses.java
@@ -3,14 +3,7 @@ package io.lcalmsky.leetcode.generate_parentheses;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GenerateParenthesesTests {
-    public static void main(String[] args) {
-        GenerateParenthesesTests generateParenthesesTests = new GenerateParenthesesTests();
-        System.out.println(generateParenthesesTests.generateParenthesis(1));
-        System.out.println(generateParenthesesTests.generateParenthesis(2));
-        System.out.println(generateParenthesesTests.generateParenthesis(3));
-    }
-
+public class GenerateParentheses {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
         dfs(result, "", n, n);
