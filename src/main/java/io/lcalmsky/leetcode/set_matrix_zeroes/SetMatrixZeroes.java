f0f10e3abc1a6cfa0fedb15c7a28d29da311b70f
Jungmin Lee <lcalmsky@gmail.com>
2019-12-23 11:41:59 +0900

    Solve the problem
    - Runtime: 1 ms (100.00%)
    - Memory Usage: 49.6 MB (15.71%)

--- src/main/java/io/lcalmsky/leetcode/set_matrix_zeroes/SetMatrixZeroes.java
+++ src/main/java/io/lcalmsky/leetcode/set_matrix_zeroes/SetMatrixZeroes.java
@@ -4,21 +4,42 @@ import java.util.Arrays;
 
 public class SetMatrixZeroes {
     public void setZeroes(int[][] matrix) {
-        int[][] copy = new int[matrix.length][matrix[0].length];
-        for (int i = 0; i < copy.length; i++) {
-            System.arraycopy(matrix[i], 0, copy[i], 0, copy[i].length);
-        }
+        boolean firstRowZero = false;
+        boolean firstColumnZero = false;
 
-        for (int i = 0; i < copy.length; i++) {
-            for (int j = 0; j < copy[i].length; j++) {
-                if (copy[i][j] != 0) continue;
-                setZeroes(matrix, i, j);
+        for (int[] ints : matrix) {
+            if (ints[0] == 0) {
+                firstColumnZero = true;
+                break;
             }
         }
-    }
 
-    private void setZeroes(int[][] matrix, int m, int n) {
-        Arrays.fill(matrix[m], 0);
-        for (int i = 0; i < matrix.length; i++) matrix[i][n] = 0;
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (firstColumnZero) for (int i = 0; i < matrix.length; i++) matrix[i][0] = 0;
+
+        if (firstRowZero) Arrays.fill(matrix[0], 0);
     }
 }
