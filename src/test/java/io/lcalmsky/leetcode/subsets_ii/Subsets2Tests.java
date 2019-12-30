7624de3bb0df8c8a5e8f14b2d7a88f7acf44f985
Jungmin Lee <lcalmsky@gmail.com>
2019-12-30 09:26:49 +0900

    Solve the problem
    - Runtime: 1 ms (99.46%)
    - Memory Usage: 37.4 MB (98.53%)

---
+++ src/test/java/io/lcalmsky/leetcode/subsets_ii/Subsets2Tests.java
@@ -0,0 +1,36 @@
+package io.lcalmsky.leetcode.subsets_ii;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class Subsets2Tests {
+
+    @Test
+    public void givenNumbers_whenGetSubsetsWithDuplicates_thenCorrect() {
+        assertAll(
+                () -> test(new int[]{1, 2, 2}, Arrays.asList(
+                        Collections.singletonList(2),
+                        Collections.singletonList(1),
+                        Arrays.asList(1, 2, 2),
+                        Arrays.asList(2, 2),
+                        Arrays.asList(1, 2),
+                        Collections.emptyList()
+                ))
+        );
+    }
+
+    private void test(int[] given, List<List<Integer>> expected) {
+        // when
+        Subsets2 subsets2 = new Subsets2();
+        List<List<Integer>> actual = subsets2.subsetsWithDup(given);
+
+        // then
+        assertThat(actual).containsAnyElementsOf(expected);
+    }
+}
