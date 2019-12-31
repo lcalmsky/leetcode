e5c74efbc76b487e10aac326f770cc81342541b0
Jungmin Lee <lcalmsky@gmail.com>
2020-01-01 01:46:58 +0900

    Solve the problem
    - Runtime: 6 ms (39,71%)
    - Memory Usage: 37.3 MB (67.27%)

---
+++ src/test/java/io/lcalmsky/leetcode/construct_binary_tree_from_inorder_and_postorder_traversal/ConstructBinaryTreeFromInorderAndPostorderTraversalTests.java
@@ -0,0 +1,25 @@
+package io.lcalmsky.leetcode.construct_binary_tree_from_inorder_and_postorder_traversal;
+
+import io.lcalmsky.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConstructBinaryTreeFromInorderAndPostorderTraversalTests {
+    @Test
+    public void givenInorderAndPostorder_whenBuildTree_theCorrect() {
+        assertAll(
+                () -> test(new int[]{9, 3, 15, 20, 7}, new int[]{9, 15, 7, 20, 3}, TreeNode.of(3, 9, 20, null, null, 15, 7))
+        );
+    }
+
+    private void test(int[] inorder, int[] postorder, TreeNode expected) {
+        // when
+        ConstructBinaryTreeFromInorderAndPostorderTraversal constructBinaryTreeFromInorderAndPostorderTraversal = new ConstructBinaryTreeFromInorderAndPostorderTraversal();
+        TreeNode actual = constructBinaryTreeFromInorderAndPostorderTraversal.buildTree(inorder, postorder);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
