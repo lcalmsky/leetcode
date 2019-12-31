f66766a4f0ecbeff3fe1a17bff278d03c0ef6b63
Jungmin Lee <lcalmsky@gmail.com>
2019-12-31 14:30:14 +0900

    Solve the problem
    - Runtime: 7 ms (46.55%)
    - Memory Usage: 36.3 MB (100.00%)

---
+++ src/test/java/io/lcalmsky/leetcode/construct_binary_tree_from_preorder_and_inorder_traversal/ConstructBinaryTreeFromPreorderAndInorderTraversalTests.java
@@ -0,0 +1,22 @@
+package io.lcalmsky.leetcode.construct_binary_tree_from_preorder_and_inorder_traversal;
+
+import io.lcalmsky.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConstructBinaryTreeFromPreorderAndInorderTraversalTests {
+    @Test
+    public void givenPreorderAndInorderArray_whenConstructBinaryTree_thenCorrect() {
+        test(new int[]{3, 9, 20, 15, 7}, new int[]{9, 3, 15, 20, 7}, TreeNode.of(3, 9, 20, null, null, 15, 7));
+    }
+
+    private void test(int[] preorder, int[] inorder, TreeNode expected) {
+        // when
+        ConstructBinaryTreeFromPreorderAndInorderTraversal constructBinaryTreeFromPreorderAndInorderTraversal = new ConstructBinaryTreeFromPreorderAndInorderTraversal();
+        TreeNode actual = constructBinaryTreeFromPreorderAndInorderTraversal.buildTree(preorder, inorder);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
