faa546590f8be73c289ea839dd16a9ae7b82da08
Jungmin Lee <lcalmsky@gmail.com>
2020-01-03 23:57:02 +0900

    Another solution

--- src/main/java/io/lcalmsky/leetcode/pascals_triangle_ii/PascalsTriangle2.java
+++ src/main/java/io/lcalmsky/leetcode/pascals_triangle_ii/PascalsTriangle2.java
@@ -1,29 +1,21 @@
 package io.lcalmsky.leetcode.pascals_triangle_ii;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class PascalsTriangle2 {
     public List<Integer> getRow(int rowIndex) {
-        if (rowIndex == 0) return Collections.singletonList(1);
+        List<Integer> result = new ArrayList<>();
 
-        int[][] arr = new int[rowIndex + 1][rowIndex + 1];
-        for (int i = 0; i <= rowIndex; i++) {
-            arr[i][0] = 1;
-            arr[0][i] = 1;
-        }
+        if (rowIndex < 0) return result;
 
+        result.add(1);
         for (int i = 1; i <= rowIndex; i++) {
-            for (int j = 1; j <= rowIndex; j++) {
-                arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+            for (int j = result.size() - 2; j >= 0; j--) {
+                result.set(j + 1, result.get(j) + result.get(j + 1));
             }
+            result.add(1);
         }
-
-        List<Integer> row = new ArrayList<>();
-
-        for (int i = 0; i < arr.length; i++) row.add(arr[rowIndex - i][i]);
-
-        return row;
+        return result;
     }
 }
