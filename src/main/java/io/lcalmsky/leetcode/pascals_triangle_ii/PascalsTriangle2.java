ecbf87015236f0425a6a2fb615e90c2739e1550e
Jungmin Lee <lcalmsky@gmail.com>
2020-01-04 00:12:43 +0900

    Another solution
    - Runtime: 0 ms (100.00%)
    - Memory Usage: 33.8 MB (6.17)

--- src/main/java/io/lcalmsky/leetcode/pascals_triangle_ii/PascalsTriangle2.java
+++ src/main/java/io/lcalmsky/leetcode/pascals_triangle_ii/PascalsTriangle2.java
@@ -7,14 +7,11 @@ public class PascalsTriangle2 {
     public List<Integer> getRow(int rowIndex) {
         List<Integer> result = new ArrayList<>();
 
-        if (rowIndex < 0) return result;
-
-        result.add(1);
-        for (int i = 1; i <= rowIndex; i++) {
-            for (int j = result.size() - 2; j >= 0; j--) {
-                result.set(j + 1, result.get(j) + result.get(j + 1));
-            }
-            result.add(1);
+        long c = 1;
+        for (int j = 0; j <= rowIndex; j++) {
+            result.add((int) c);
+            c *= rowIndex - j;
+            c /= j + 1;
         }
         return result;
     }
