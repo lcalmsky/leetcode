fdd6b0ca2620ecfcf68b045f58b19d072b222739
Jungmin Lee <lcalmsky@gmail.com>
2020-01-07 10:09:32 +0900

    Solve the problem
    - Runtime: 3 ms (52.90%)
    - Memory Usage: 39.6 MB (100.00%)

---
+++ src/main/java/io/lcalmsky/leetcode/candy/Candy.java
@@ -0,0 +1,24 @@
+package io.lcalmsky.leetcode.candy;
+
+public class Candy {
+    public int candy(int[] ratings) {
+        if (ratings == null || ratings.length == 0) return 0;
+        int[] candies = new int[ratings.length];
+        candies[0] = 1;
+
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) candies[i] = candies[i - 1] + 1;
+            else candies[i] = 1;
+        }
+
+        int sum = candies[ratings.length - 1];
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            int current = 1;
+            if (ratings[i] > ratings[i + 1]) current = candies[i + 1] + 1;
+            sum += Math.max(current, candies[i]);
+            candies[i] = current;
+        }
+
+        return sum;
+    }
+}
