bb7a1145dc4a1dad7855eb5227754630ef436490
Jungmin Lee <lcalmsky@gmail.com>
2020-01-07 09:21:14 +0900

    Solve the problem
    - Runtime: 0 ms (100.00%)
    - Memory Usage: 37 MB (100.00%)

---
+++ src/main/java/io/lcalmsky/leetcode/gas_station/GasStation.java
@@ -0,0 +1,18 @@
+package io.lcalmsky.leetcode.gas_station;
+
+public class GasStation {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int sum = 0, total = 0, start = 0, remaining;
+        for (int i = 0; i < gas.length; i++) {
+            remaining = gas[i] - cost[i];
+            if (sum >= 0) {
+                sum += remaining;
+            } else {
+                sum = remaining;
+                start = i;
+            }
+            total += remaining;
+        }
+        return total >= 0 ? start : -1;
+    }
+}
