7d6338865c8d250f078cfc66784b03bad0904807
Jungmin Lee <lcalmsky@gmail.com>
2020-01-09 08:29:07 +0900

    Solve the problem
    - Runtime: 0 ms (100.00%)
    - Memory Usage: 34.8 MB (100.00%)

---
+++ src/main/java/io/lcalmsky/leetcode/binary_tree_preorder_traversal/BinaryTreePreorderTraversal.java
@@ -0,0 +1,21 @@
+package io.lcalmsky.leetcode.binary_tree_preorder_traversal;
+
+import io.lcalmsky.leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePreorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> order = new ArrayList<>();
+        preorder(root, order);
+        return order;
+    }
+
+    private void preorder(TreeNode root, List<Integer> order) {
+        if (root == null) return;
+        order.add(root.val);
+        preorder(root.left, order);
+        preorder(root.right, order);
+    }
+}
---
+++ src/test/java/io/lcalmsky/leetcode/binary_tree_preorder_traversal/BinaryTreePreorderTraversalTests.java
@@ -0,0 +1,28 @@
+package io.lcalmsky.leetcode.binary_tree_preorder_traversal;
+
+import io.lcalmsky.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BinaryTreePreorderTraversalTests {
+    @Test
+    public void givenTreeNode_whenPreorderTraverse_thenCorrect() {
+        assertAll(
+                () -> test(TreeNode.of(1, null, 2, 3), Arrays.asList(1, 2, 3))
+        );
+    }
+
+    private void test(TreeNode given, List<Integer> expected) {
+        // when
+        BinaryTreePreorderTraversal binaryTreePreorderTraversal = new BinaryTreePreorderTraversal();
+        List<Integer> actual = binaryTreePreorderTraversal.preorderTraversal(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
