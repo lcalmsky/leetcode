1c587f6920e1d5024712a67cc424e12444cc2c23
Jungmin Lee <lcalmsky@gmail.com>
2020-01-14 08:18:13 +0900

    Solve the problem
    - Runtime: 0 ms (100.00%)
    - Memory Usage: 41.5 MB (6.25%)

---
+++ src/test/java/io/lcalmsky/leetcode/find_minimum_in_rotated_sorted_array_ii/FindMinimumInRotatedSortedArray2Tests.java
@@ -0,0 +1,25 @@
+package io.lcalmsky.leetcode.find_minimum_in_rotated_sorted_array_ii;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FindMinimumInRotatedSortedArray2Tests {
+    @Test
+    public void givenRotatedSortedWithDuplicatesArray_whenFindMinimum_thenCorrect() {
+        assertAll(
+                () -> test(new int[]{1, 3, 5}, 1),
+                () -> test(new int[]{2, 2, 2, 0, 1}, 0)
+        );
+    }
+
+    private void test(int[] given, int expected) {
+        // when
+        FindMinimumInRotatedSortedArray2 findMinimumInRotatedSortedArray2 = new FindMinimumInRotatedSortedArray2();
+        int actual = findMinimumInRotatedSortedArray2.findMin(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
