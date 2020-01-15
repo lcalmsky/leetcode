470bce0ebb284e09576cb7598c56e9fecb9e4432
Jungmin Lee <lcalmsky@gmail.com>
2020-01-15 11:19:44 +0900

    Solve the problem
    - Runtime: 0 ms (100.00%)
    - Memory Usage: 38.7 MB (17.91%)

--- src/main/java/io/lcalmsky/leetcode/two_sum_ii_input_array_is_sorted/TwoSumInputArrayIsSorted.java
+++ src/main/java/io/lcalmsky/leetcode/two_sum_ii_input_array_is_sorted/TwoSumInputArrayIsSorted.java
@@ -2,12 +2,17 @@ package io.lcalmsky.leetcode.two_sum_ii_input_array_is_sorted;
 
 public class TwoSumInputArrayIsSorted {
     public int[] twoSum(int[] numbers, int target) {
-        int sum = 0;
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] + numbers[j] == target) return new int[]{i + 1, j + 1};
-            }
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int x = numbers[left] + numbers[right];
+            if (x < target) ++left;
+            else if (x > target) right--;
+            else return new int[]{left + 1, right + 1};
         }
+
         return null;
     }
 }
