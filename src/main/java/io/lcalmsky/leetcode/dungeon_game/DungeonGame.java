be3313784fa59799c361e78161caac3e48a45a5e
Jungmin Lee <lcalmsky@gmail.com>
2020-01-21 08:39:34 +0900

    Solve the problem
    - Runtime: 2 ms (43.72%)
    - Memory Usage: 51.5 MB (5.88%)

---
+++ src/main/java/io/lcalmsky/leetcode/dungeon_game/DungeonGame.java
@@ -0,0 +1,27 @@
+package io.lcalmsky.leetcode.dungeon_game;
+
+public class DungeonGame {
+    public int calculateMinimumHp(int[][] dungeon) {
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+
+        //init dp table
+        int[][] dp = new int[m][n];
+
+        dp[m - 1][n - 1] = Math.max(1 - dungeon[m - 1][n - 1], 1);
+
+        for (int i = m - 2; i >= 0; i--) dp[i][n - 1] = Math.max(dp[i + 1][n - 1] - dungeon[i][n - 1], 1);
+
+        for (int j = n - 2; j >= 0; j--) dp[m - 1][j] = Math.max(dp[m - 1][j + 1] - dungeon[m - 1][j], 1);
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                int down = Math.max(dp[i + 1][j] - dungeon[i][j], 1);
+                int right = Math.max(dp[i][j + 1] - dungeon[i][j], 1);
+                dp[i][j] = Math.min(right, down);
+            }
+        }
+
+        return dp[0][0];
+    }
+}
