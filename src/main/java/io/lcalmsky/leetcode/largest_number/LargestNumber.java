5a586d8a2d89bc77a56f82a0e5671df561650870
Jungmin Lee <lcalmsky@gmail.com>
2020-01-21 09:21:13 +0900

    Solve the problem
    - Runtime: 7 ms (21.71%)
    - Memory Usage: 42.3 MB (6.67%)

---
+++ src/main/java/io/lcalmsky/leetcode/largest_number/LargestNumber.java
@@ -0,0 +1,20 @@
+package io.lcalmsky.leetcode.largest_number;
+
+import java.util.Arrays;
+
+public class LargestNumber {
+    public String largestNumber(int[] nums) {
+
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) strs[i] = String.valueOf(nums[i]);
+
+        Arrays.sort(strs, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
+
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) sb.append(str);
+
+        while (sb.charAt(0) == '0' && sb.length() > 1) sb.deleteCharAt(0);
+
+        return sb.toString();
+    }
+}
