59d0aed4dd43583853b28f29c0ed5f44d4f878f4
Jungmin Lee <lcalmsky@gmail.com>
2020-01-28 08:49:57 +0900

    Solve the problem
    - Runtime: 0 ms (100.00%)
    - Memory Usage: 41.8 MB (6.35%)

---
+++ src/main/java/io/lcalmsky/leetcode/remove_linked_list_elements/RemoveLinkedListElements.java
@@ -0,0 +1,18 @@
+package io.lcalmsky.leetcode.remove_linked_list_elements;
+
+import io.lcalmsky.leetcode.ListNode;
+
+public class RemoveLinkedListElements {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode root = new ListNode(0);
+        root.next = head;
+        ListNode p = root;
+
+        while (p.next != null) {
+            if (p.next.val == val) p.next = p.next.next;
+            else p = p.next;
+        }
+
+        return root.next;
+    }
+}
