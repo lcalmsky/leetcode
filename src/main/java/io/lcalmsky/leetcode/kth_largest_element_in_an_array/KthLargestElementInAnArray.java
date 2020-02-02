fc22f343728394e06c473fe3d5ff2fbbf1652971
이정민(Jungmin Lee)/Payment개발팀/11ST <jaime.lee@sk.com>
2020-02-02 17:50:38 +0900

    Solve the problem
    - Runtime: 1 ms (99.26%)
    - Memory Usage: 41.1 MB (5.18%)

--- src/main/java/io/lcalmsky/leetcode/kth_largest_element_in_an_array/KthLargestElementInAnArray.java
+++ src/main/java/io/lcalmsky/leetcode/kth_largest_element_in_an_array/KthLargestElementInAnArray.java
@@ -1,14 +1,10 @@
 package io.lcalmsky.leetcode.kth_largest_element_in_an_array;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class KthLargestElementInAnArray {
     public int findKthLargest(int[] nums, int k) {
-        List<Integer> list = new ArrayList<>();
-        for (int num : nums) list.add(num);
-        Collections.sort(list);
-        return list.get(list.size() - k);
+        Arrays.sort(nums);
+        return nums[nums.length - k];
     }
 }
