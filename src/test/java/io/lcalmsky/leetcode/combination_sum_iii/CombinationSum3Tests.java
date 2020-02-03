957541dccc169e0600097f955a2a0063c12c65c1
이정민(Jungmin Lee)/Payment개발팀/11ST <jaime.lee@sk.com>
2020-02-03 09:14:08 +0900

    Solve the problem
    - Runtime: 0 ms (100.00%)
    - Memory Usage: 36.3 MB (6.00%)

---
+++ src/test/java/io/lcalmsky/leetcode/combination_sum_iii/CombinationSum3Tests.java
@@ -0,0 +1,33 @@
+package io.lcalmsky.leetcode.combination_sum_iii;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class CombinationSum3Tests {
+    @Test
+    public void givenNumberAndSum_whenFindCombinations_thenCorrect() {
+        assertAll(
+                () -> test(3, 7, Collections.singletonList(Arrays.asList(1, 2, 4))),
+                () -> test(3, 9, Arrays.asList(
+                        Arrays.asList(1, 2, 6),
+                        Arrays.asList(1, 3, 5),
+                        Arrays.asList(2, 3, 4))
+                )
+        );
+    }
+
+    private void test(int k, int n, List<List<Integer>> expected) {
+        // when
+        CombinationSum3 combinationSum3 = new CombinationSum3();
+        List<List<Integer>> actual = combinationSum3.combinationSum3(k, n);
+
+        // then
+        assertThat(actual).containsOnlyElementsOf(expected);
+    }
+}
