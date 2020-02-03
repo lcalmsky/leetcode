f1afaf49094e135653688d52b182223de412ddbf
Jungmin Lee <lcalmsky@gmail.com>
2020-02-04 08:34:57 +0900

    Solve the problem
    - Runtime: 4 ms (95.82%)
    - Memory Usage: 46.7 MB (5.17%)

---
+++ src/test/java/io/lcalmsky/leetcode/contains_duplicates/ContainsDuplicateTests.java
@@ -0,0 +1,26 @@
+package io.lcalmsky.leetcode.contains_duplicates;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ContainsDuplicateTests {
+    @Test
+    public void givenNumbers_whenCheckContainDuplicate_thenCorrect() {
+        assertAll(
+                () -> test(new int[]{1, 2, 3, 1}, true),
+                () -> test(new int[]{1, 2, 3, 4}, false),
+                () -> test(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}, true)
+        );
+    }
+
+    private void test(int[] given, boolean expected) {
+        // when
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+        boolean actual = containsDuplicate.containsDuplicate(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
