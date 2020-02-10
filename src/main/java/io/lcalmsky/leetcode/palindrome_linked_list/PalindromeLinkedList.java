b7fd9daecc91d60bf9a2bf66753be95205d2e3ea
Jungmin Lee <lcalmsky@gmail.com>
2020-02-11 08:45:06 +0900

    Solve the problem (recursive way)
    - Runtime: 1 ms (98.20%)
    - Memory Usage: 46 MB (6.10%)

--- src/main/java/io/lcalmsky/leetcode/palindrome_linked_list/PalindromeLinkedList.java
+++ src/main/java/io/lcalmsky/leetcode/palindrome_linked_list/PalindromeLinkedList.java
@@ -2,21 +2,33 @@ package io.lcalmsky.leetcode.palindrome_linked_list;
 
 import io.lcalmsky.leetcode.ListNode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class PalindromeLinkedList {
-    public boolean isPalindrome(ListNode head) {
-        List<Integer> list = new ArrayList<>();
-        while (head != null) {
-            list.add(head.val);
-            head = head.next;
-        }
-        int left = 0, right = list.size() - 1;
-        while (left < right) {
-            if (!list.get(left++).equals(list.get(right--))) return false;
-        }
+    private ListNode left;
+//    public boolean isPalindrome(ListNode head) {
+//        List<Integer> list = new ArrayList<>();
+//        while (head != null) {
+//            list.add(head.val);
+//            head = head.next;
+//        }
+//        int left = 0, right = list.size() - 1;
+//        while (left < right) {
+//            if (!list.get(left++).equals(list.get(right--))) return false;
+//        }
+//
+//        return true;
+//    }
 
-        return true;
+    public boolean isPalindrome(ListNode head) {
+        left = head;
+        return helper(head);
+    }
+
+    private boolean helper(ListNode right) {
+        if (right == null) return true;
+        boolean x = helper(right.next);
+        if (!x) return false;
+        boolean y = left.val == right.val;
+        left = left.next;
+        return y;
     }
 }
