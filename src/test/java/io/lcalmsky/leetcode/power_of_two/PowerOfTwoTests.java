516ce5d2294cf7e4f909e864ca57c119308e9b71
Jungmin Lee <lcalmsky@gmail.com>
2020-02-11 08:27:21 +0900

    Solve the problem
    - Runtime: 1 ms (100.00%)
    - Memory Usage: 36.9 MB (7.32%)

---
+++ src/test/java/io/lcalmsky/leetcode/power_of_two/PowerOfTwoTests.java
@@ -0,0 +1,26 @@
+package io.lcalmsky.leetcode.power_of_two;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PowerOfTwoTests {
+    @Test
+    public void givenNumber_whenCheckIsPowerOfTwo_thenCorrect() {
+        assertAll(
+                () -> test(1, true),
+                () -> test(16, true),
+                () -> test(218, false)
+        );
+    }
+
+    private void test(int given, boolean expected) {
+        // when
+        PowerOfTwo powerOfTwo = new PowerOfTwo();
+        boolean actual = powerOfTwo.isPowerOfTwo(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
