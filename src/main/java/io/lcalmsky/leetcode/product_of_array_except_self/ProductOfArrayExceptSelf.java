e2811b58d509e52b31317ab1cc359b447078e5f8
Jungmin Lee <lcalmsky@gmail.com>
2020-02-12 08:59:28 +0900

    Solve the problem (recursive way)
    - Runtime: 1 ms (100.00%)
    - Memory Usage: 48.2 MB (5.51%)

---
+++ src/main/java/io/lcalmsky/leetcode/product_of_array_except_self/ProductOfArrayExceptSelf.java
@@ -0,0 +1,17 @@
+package io.lcalmsky.leetcode.product_of_array_except_self;
+
+public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        result[nums.length - 1] = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            result[i] = result[i + 1] * nums[i + 1];
+        }
+        int left = 1;
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = result[i] * left;
+            left = left * nums[i];
+        }
+        return result;
+    }
+}
