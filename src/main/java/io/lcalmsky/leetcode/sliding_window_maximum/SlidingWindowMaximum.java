63e3eaaab234ea1d424f552f269abef1f5c3ff08
Jungmin Lee <lcalmsky@gmail.com>
2020-02-13 13:49:37 +0900

    Solve the problem (recursive way)
    - Runtime: 8 ms (86.92%)
    - Memory Usage: 47.5 MB (6.25%)

--- src/main/java/io/lcalmsky/leetcode/sliding_window_maximum/SlidingWindowMaximum.java
+++ src/main/java/io/lcalmsky/leetcode/sliding_window_maximum/SlidingWindowMaximum.java
@@ -1,16 +1,28 @@
 package io.lcalmsky.leetcode.sliding_window_maximum;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class SlidingWindowMaximum {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums == null || nums.length == 0) return nums;
+        if (nums == null || nums.length == 0)
+            return new int[0];
+
         int[] result = new int[nums.length - k + 1];
 
-        for (int i = 0; i < result.length; i++) {
-            int max = Integer.MIN_VALUE;
-            for (int j = i; j < i + k; j++) {
-                max = Math.max(max, nums[j]);
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!deque.isEmpty() && deque.peekFirst() == i - k)
+                deque.poll();
+
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.removeLast();
             }
-            result[i] = max;
+
+            deque.offer(i);
+
+            if (i + 1 >= k)
+                result[i + 1 - k] = nums[deque.peek()];
         }
 
         return result;
--- src/test/java/io/lcalmsky/leetcode/sliding_window_maximum/SlidingWindowMaximumTests.java
+++ src/test/java/io/lcalmsky/leetcode/sliding_window_maximum/SlidingWindowMaximumTests.java
@@ -10,7 +10,9 @@ public class SlidingWindowMaximumTests {
     public void givenArray_whenFindMaximumWhileSliding_thenCorrect() {
         assertAll(
                 () -> test(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3, new int[]{3, 3, 5, 5, 6, 7}),
-                () -> test(new int[]{}, 0, new int[]{})
+                () -> test(new int[]{}, 0, new int[]{}),
+                () -> test(new int[]{1, -1}, 1, new int[]{1, -1}),
+                () -> test(new int[]{7, 2, 4}, 2, new int[]{7, 4})
         );
     }
 
