ae68defbafdd0ef292402ebc4ae138f59d0f9e2d
Jungmin Lee <lcalmsky@gmail.com>
2020-02-14 11:27:38 +0900

    Solve the problem (recursive way)
    - Runtime: 5 ms (99.88%)
    - Memory Usage: 42.3 MB (100.00%)

---
+++ src/test/java/io/lcalmsky/leetcode/search_a_2d_matrix_ii/SearchA2dMatrix2Tests.java
@@ -0,0 +1,34 @@
+package io.lcalmsky.leetcode.search_a_2d_matrix_ii;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SearchA2dMatrix2Tests {
+    @Test
+    public void given2dMatrix_whenFindTarget_thenReturnsTargetExists() {
+        final int[][] matrix = {
+                {1, 4, 7, 11, 15},
+                {1, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30},
+        };
+        assertAll(
+                () -> test(new int[][]{}, 0, false),
+                () -> test(matrix, 5, true),
+                () -> test(matrix, 20, false)
+
+        );
+    }
+
+    private void test(int[][] matrix, int target, boolean expected) {
+        // when
+        SearchA2dMatrix2 searchA2dMatrix2 = new SearchA2dMatrix2();
+        boolean actual = searchA2dMatrix2.searchMatrix(matrix, target);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
