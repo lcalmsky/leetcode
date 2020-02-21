6c367bb282eafaacb65b3cf19790dc53b4456430
Jungmin Lee <lcalmsky@gmail.com>
2020-02-21 14:01:51 +0900

    Another solution
    - Runtime: 1 ms (100.00%)
    - Memory Usage: 36.8 MB (6.67%)

--- src/main/java/io/lcalmsky/leetcode/add_digits/AddDigits.java
+++ src/main/java/io/lcalmsky/leetcode/add_digits/AddDigits.java
@@ -2,14 +2,6 @@ package io.lcalmsky.leetcode.add_digits;
 
 public class AddDigits {
     public int addDigits(int num) {
-        while (num / 10 != 0) {
-            String s = String.valueOf(num);
-            int sum = 0;
-            for (int i = 0; i < s.length(); i++) {
-                sum += s.charAt(i) - '0';
-            }
-            num = sum;
-        }
-        return num;
+        return num - 9 * ((num - 1) / 9);
     }
 }
