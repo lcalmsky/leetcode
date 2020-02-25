5f7a901850525f32957fd3015541ab2e73e23616
Jungmin Lee <lcalmsky@gmail.com>
2020-02-25 11:56:24 +0900

    Solve the problem
    - Runtime: 44 ms (36.96%)
    - Memory Usage: 38.8 MB (13.89%)

---
+++ src/test/java/io/lcalmsky/leetcode/perfect_squares/PerfectSquaresTests.java
@@ -0,0 +1,25 @@
+package io.lcalmsky.leetcode.perfect_squares;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PerfectSquaresTests {
+    @Test
+    public void givenNumber_whenFindLeastNumber_thenCorrect() {
+        assertAll(
+                () -> test(12, 3),
+                () -> test(13, 2)
+        );
+    }
+
+    private void test(int given, int expected) {
+        // when
+        PerfectSquares perfectSquares = new PerfectSquares();
+        int actual = perfectSquares.numSquares(given);
+
+        // then
+        assertEquals(actual, expected);
+    }
+}
