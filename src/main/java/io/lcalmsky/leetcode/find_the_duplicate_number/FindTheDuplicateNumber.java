c6ea2145955a5152205b779a5a8aecbe522e46b7
Jungmin Lee <lcalmsky@gmail.com>
2020-02-27 16:27:21 +0900

    Solve the problem
    - Runtime: 2 ms (51.12%)
    - Memory Usage: 42.7 MB (5.09%)

---
+++ src/main/java/io/lcalmsky/leetcode/find_the_duplicate_number/FindTheDuplicateNumber.java
@@ -0,0 +1,14 @@
+package io.lcalmsky.leetcode.find_the_duplicate_number;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindTheDuplicateNumber {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (!set.add(i)) return i;
+        }
+        return -1;
+    }
+}
