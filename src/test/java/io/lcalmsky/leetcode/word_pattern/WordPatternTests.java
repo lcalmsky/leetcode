4724c81da702f2473d02b9a1e0c85f1ae76e2095
Jungmin Lee <lcalmsky@gmail.com>
2020-02-28 13:07:14 +0900

    Solve the problem
    - Runtime: 0 ms (100.00%)
    - Memory Usage: 37.5 MB (5.41%)

---
+++ src/test/java/io/lcalmsky/leetcode/word_pattern/WordPatternTests.java
@@ -0,0 +1,27 @@
+package io.lcalmsky.leetcode.word_pattern;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WordPatternTests {
+    @Test
+    public void givenPatternAndWords_whenCheckPattern_thenCorrect() {
+        assertAll(
+                () -> test("abba", "dog cat cat dog", true),
+                () -> test("abba", "dog cat cat fish", false),
+                () -> test("aaaa", "dog cat cat dog", false),
+                () -> test("abba", "dog dog dog dog", false)
+        );
+    }
+
+    private void test(String pattern, String words, boolean expected) {
+        // when
+        WordPattern wordPattern = new WordPattern();
+        boolean actual = wordPattern.wordPattern(pattern, words);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
