6251d4fbd7ca45ee819b9d52bf8f8383c45d2319
Jungmin Lee <lcalmsky@gmail.com>
2020-03-03 10:40:35 +0900

    Solve the problem
    - Runtime: 10 ms (100.00%)
    - Memory Usage: 47.4 MB (9.09%)

--- src/main/java/io/lcalmsky/leetcode/range_sum_query_2d_immutable/NumMatrix.java
+++ src/main/java/io/lcalmsky/leetcode/range_sum_query_2d_immutable/NumMatrix.java
@@ -2,19 +2,52 @@ package io.lcalmsky.leetcode.range_sum_query_2d_immutable;
 
 public class NumMatrix {
 
-    private final int[][] matrix;
+    private int[][] sum;
 
     public NumMatrix(int[][] matrix) {
-        this.matrix = matrix;
-    }
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        sum = new int[m][n];
 
-    public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
-        for (int i = row1; i <= row2; i++) {
-            for (int j = col1; j <= col2; j++) {
-                sum += matrix[i][j];
+        for (int i = 0; i < m; i++) {
+            int sumRow = 0;
+            for (int j = 0; j < n; j++) {
+                if (i == 0) {
+                    sumRow += matrix[i][j];
+                    sum[i][j] = sumRow;
+                } else {
+                    sumRow += matrix[i][j];
+                    sum[i][j] = sumRow + sum[i - 1][j];
+                }
+
             }
         }
-        return sum;
+    }
+
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        if (this.sum == null)
+            return 0;
+
+        int result;
+
+        if (row1 == 0 && col1 == 0) {
+            result = sum[row2][col2];
+        } else if (row1 == 0) {
+            result = sum[row2][col2]
+                    - sum[row2][col1 - 1];
+
+        } else if (col1 == 0) {
+            result = sum[row2][col2]
+                    - sum[row1 - 1][col2];
+        } else {
+            result = sum[row2][col2]
+                    - sum[row1 - 1][col2]
+                    - sum[row2][col1 - 1]
+                    + sum[row1 - 1][col1 - 1];
+        }
+
+        return result;
     }
 }
