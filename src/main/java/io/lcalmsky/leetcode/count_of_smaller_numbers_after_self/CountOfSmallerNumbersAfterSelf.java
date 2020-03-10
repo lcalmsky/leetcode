5d59f8b572e16f776501334cb473e3094cbd1782
Jungmin Lee <lcalmsky@gmail.com>
2020-03-10 11:37:29 +0900

    Solve the problem
    - binary search solution
    - Runtime: 4 ms (94.38%)
    - Memory Usage: 42.3 MB (5.55%)

--- src/main/java/io/lcalmsky/leetcode/count_of_smaller_numbers_after_self/CountOfSmallerNumbersAfterSelf.java
+++ src/main/java/io/lcalmsky/leetcode/count_of_smaller_numbers_after_self/CountOfSmallerNumbersAfterSelf.java
@@ -6,17 +6,67 @@ import java.util.List;
 
 public class CountOfSmallerNumbersAfterSelf {
     public List<Integer> countSmaller(int[] nums) {
+
         if (nums == null || nums.length == 0) return Collections.emptyList();
+
         List<Integer> result = new ArrayList<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            int cnt = 0;
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] > nums[j]) cnt++;
-            }
-            result.add(cnt);
-        }
+        Node root = new Node(nums[nums.length - 1]);
+        root.count = 1;
+        result.add(0);
+
+        for (int i = nums.length - 2; i >= 0; i--) result.add(insertNode(root, nums[i]));
+
+        Collections.reverse(result);
 
         return result;
+    }
+
+    public int insertNode(Node root, int value) {
+        Node p = root;
+        int result = 0;
+
+        while (p != null) {
+            if (value > p.value) {
+                result += p.count + p.numLeft;
+                if (p.right == null) {
+                    Node t = new Node(value);
+                    t.count = 1;
+                    p.right = t;
+                    return result;
+                } else {
+                    p = p.right;
+                }
+            } else if (value == p.value) {
+                p.count++;
+                return result + p.numLeft;
+            } else {
+                p.numLeft++;
+
+                if (p.left == null) {
+                    Node t = new Node(value);
+                    t.count = 1;
+                    p.left = t;
+                    return result;
+                } else {
+                    p = p.left;
+                }
+            }
+        }
+
+        return 0;
+    }
+}
+
+class Node {
+    Node left;
+    Node right;
+
+    int value;
+    int count;
+    int numLeft;
+
+    public Node(int value) {
+        this.value = value;
     }
 }
