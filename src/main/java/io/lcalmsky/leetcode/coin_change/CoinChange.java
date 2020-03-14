74b9d5bb29983ba2fc18f9e23e58b81742ad8986
Jungmin Lee <lcalmsky@gmail.com>
2020-03-15 01:09:14 +0900

    Solve the problem
    - Runtime: 14 ms (43.86%)
    - Memory Usage: 41.1 MB (5.33%)

---
+++ src/main/java/io/lcalmsky/leetcode/coin_change/CoinChange.java
@@ -0,0 +1,39 @@
+package io.lcalmsky.leetcode.coin_change;
+
+import java.util.Arrays;
+
+/**
+ * You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: coins = [1, 2, 5], amount = 11
+ * Output: 3
+ * Explanation: 11 = 5 + 5 + 1
+ * Example 2:
+ * <p>
+ * Input: coins = [2], amount = 3
+ * Output: -1
+ * Note:
+ * You may assume that you have an infinite number of each kind of coin.
+ */
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) return 0;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i == coin) dp[i] = 1;
+                else if (i > coin) {
+                    if (dp[i - coin] == Integer.MAX_VALUE) continue;
+                    dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+                }
+            }
+        }
+        if (dp[amount] == Integer.MAX_VALUE) return -1;
+
+        return dp[amount];
+    }
+}
---
+++ src/test/java/io/lcalmsky/leetcode/coin_change/CoinChangeTests.java
@@ -0,0 +1,25 @@
+package io.lcalmsky.leetcode.coin_change;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CoinChangeTests {
+    @Test
+    public void givenCoins_whenGetFewestNumberOfCoins_thenCorrect() {
+        assertAll(
+                () -> test(new int[]{1, 2, 5}, 11, 3),
+                () -> test(new int[]{2}, 3, -1)
+        );
+    }
+
+    private void test(int[] coins, int amount, int expected) {
+        // when
+        CoinChange coinChange = new CoinChange();
+        int actual = coinChange.coinChange(coins, amount);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
