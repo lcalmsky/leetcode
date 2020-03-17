12c146d8d5aaef3812ad57417d06b897731a75bb
Jungmin Lee <lcalmsky@gmail.com>
2020-03-17 15:02:11 +0900

    Solve the problem
    - Runtime: 9 ms (42.01%)
    - Memory Usage: 42 MB (14.29%)

--- src/main/java/io/lcalmsky/leetcode/longest_increasing_path_in_a_matrix/LongestIncreasingPathInAMatrix.java
+++ src/main/java/io/lcalmsky/leetcode/longest_increasing_path_in_a_matrix/LongestIncreasingPathInAMatrix.java
@@ -2,21 +2,26 @@ package io.lcalmsky.leetcode.longest_increasing_path_in_a_matrix;
 
 public class LongestIncreasingPathInAMatrix {
     public int longestIncreasingPath(int[][] matrix) {
-        int[] max = new int[1];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                dfs(matrix, i, j, max, 1);
+        int result = 0;
+        if (matrix == null || matrix.length == 0) return result;
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] mem = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int t = helper(matrix, mem, i, j);
+                result = Math.max(result, t);
             }
         }
 
-        return max[0];
+        return result;
     }
 
-    public void dfs(int[][] matrix, int i, int j, int[] max, int len) {
-        max[0] = Math.max(max[0], len);
-
-        int m = matrix.length;
-        int n = matrix[0].length;
+    private int helper(int[][] matrix, int[][] mem, int i, int j) {
+        if (mem[i][j] > 0) {
+            return mem[i][j];
+        }
 
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, 1, 0, -1};
@@ -25,9 +30,14 @@ public class LongestIncreasingPathInAMatrix {
             int x = i + dx[k];
             int y = j + dy[k];
 
-            if (x >= 0 && x < m && y >= 0 && y < n && matrix[x][y] > matrix[i][j]) {
-                dfs(matrix, x, y, max, len + 1);
+            if (x >= 0 && y >= 0
+                    && x < matrix.length
+                    && y < matrix[0].length
+                    && matrix[x][y] > matrix[i][j]) {
+                mem[i][j] = Math.max(mem[i][j], helper(matrix, mem, x, y));
             }
         }
+
+        return ++mem[i][j];
     }
 }
