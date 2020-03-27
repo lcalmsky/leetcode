fd7f172d26ee15fe49876c942440e53f039fb425
Jungmin Lee <lcalmsky@gmail.com>
2020-03-27 14:29:52 +0900

    Solve the problem
    - Runtime: 0 ms (100.00%)
    - Memory Usage: 39.5 MB (58.33%)

---
+++ src/test/java/io/lcalmsky/leetcode/house_robber_iii/HouseRobber3Tests.java
@@ -0,0 +1,26 @@
+package io.lcalmsky.leetcode.house_robber_iii;
+
+import io.lcalmsky.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HouseRobber3Tests {
+    @Test
+    public void givenTree_whenRob_thenGetMaximumAmountOfMoney() {
+        assertAll(
+                () -> test(TreeNode.of(3, 2, 3, null, 3, null, 1), 7),
+                () -> test(TreeNode.of(3, 4, 5, 1, 3, null, 1), 9)
+        );
+    }
+
+    private void test(TreeNode given, int expected) {
+        // when
+        HouseRobber3 houseRobber3 = new HouseRobber3();
+        int actual = houseRobber3.rob(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
