ea3ebd552ebe6aa745d061750643d927aa2b2f85
Jungmin Lee <lcalmsky@gmail.com>
2020-03-28 02:48:02 +0900

    Solve the problem
    - Runtime: 1 ms (99.45%)
    - Memory Usage: 44 MB (5.88%)

--- src/main/java/io/lcalmsky/leetcode/counting_bits/CountingBits.java
+++ src/main/java/io/lcalmsky/leetcode/counting_bits/CountingBits.java
@@ -34,11 +34,10 @@ public class CountingBits {
         for (int i = 1; i <= num; i++) {
             if (i == pow) {
                 result[i] = 1;
-                pow <<= 1;
+                pow *= 2;
                 p = 1;
             } else {
-                result[i] = result[p] + 1;
-                p++;
+                result[i] = result[p++] + 1;
             }
         }
 
