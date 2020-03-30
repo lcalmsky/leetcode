b730bf64a65b79f722a862c3c2ec3161e3bae16f
Jungmin Lee <lcalmsky@gmail.com>
2020-03-31 01:31:00 +0900

    Solve the problem
    - Runtime: 1 ms (50.32%)
    - Memory Usage: 36.6 MB (14.29%)

---
+++ src/main/java/io/lcalmsky/leetcode/integer_break/IntegerBreak.java
@@ -0,0 +1,33 @@
+package io.lcalmsky.leetcode.integer_break;
+
+/**
+ * <pre>
+ * Given a positive integer n, break it into the sum of at least two positive integers and maximize the product of those integers. Return the maximum product you can get.
+ *
+ * Example 1:
+ *
+ * Input: 2
+ * Output: 1
+ * Explanation: 2 = 1 + 1, 1 × 1 = 1.
+ *
+ * Example 2:
+ *
+ * Input: 10
+ * Output: 36
+ * Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+ *
+ * Note: You may assume that n is not less than 2 and not larger than 58.
+ * </pre>
+ */
+public class IntegerBreak {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < i + 1; j++) {
+                if (i + j <= n)
+                    dp[i + j] = Math.max(Math.max(dp[i], i) * Math.max(dp[j], j), dp[i + j]);
+            }
+        }
+        return dp[n];
+    }
+}
