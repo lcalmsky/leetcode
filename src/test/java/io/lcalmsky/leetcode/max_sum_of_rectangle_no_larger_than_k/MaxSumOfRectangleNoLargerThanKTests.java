0dfa5b5abc48200efc8dd997997dfa76ee2042ee
Jungmin Lee <lcalmksy@gmail.com>
2020-04-09 10:58:15 +0900

    Solve the problem
    - Runtime: 139 ms (88.89%)
    - Memory Usage: 39.4 MB (100.00%)

---
+++ src/test/java/io/lcalmsky/leetcode/max_sum_of_rectangle_no_larger_than_k/MaxSumOfRectangleNoLargerThanKTests.java
@@ -0,0 +1,27 @@
+package io.lcalmsky.leetcode.max_sum_of_rectangle_no_larger_than_k;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MaxSumOfRectangleNoLargerThanKTests {
+    @Test
+    public void givenMatrix_findMaxSumOfARectangleNoLargerThanK_thenCorrect() {
+        assertAll(
+                () -> test(new int[][]{
+                        {1, 0, 1},
+                        {0, -2, 3}
+                }, 2, 2)
+        );
+    }
+
+    private void test(int[][] given, int k, int expected) {
+        // when
+        MaxSumOfRectangleNoLargerThanK maxSumOfRectangleNoLargerThanK = new MaxSumOfRectangleNoLargerThanK();
+        int actual = maxSumOfRectangleNoLargerThanK.maxSumSubmatrix(given, k);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
