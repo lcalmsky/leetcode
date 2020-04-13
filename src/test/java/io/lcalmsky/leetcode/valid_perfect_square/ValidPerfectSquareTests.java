d0ed266fa8a7956e1eb49e8dbf6e6630a1a90c3c
Jaime <lcalmsky@gmail.com>
2020-04-13 13:53:44 +0900

    Solve the problem
    - Runtime: 0 ms (100.00%)
    - Memory Usage: 36.4 MB (6.12%)

---
+++ src/test/java/io/lcalmsky/leetcode/valid_perfect_square/ValidPerfectSquareTests.java
@@ -0,0 +1,25 @@
+package io.lcalmsky.leetcode.valid_perfect_square;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ValidPerfectSquareTests {
+    @Test
+    public void givenInteger_whenFindItIsPerfectSquare_thenCorrect() {
+        assertAll(
+                () -> test(16, true),
+                () -> test(14, false)
+        );
+    }
+
+    private void test(int given, boolean expected) {
+        // when
+        ValidPerfectSquare validPerfectSquare = new ValidPerfectSquare();
+        boolean actual = validPerfectSquare.isPerfectSquare(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
