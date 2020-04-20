f54dcae148a0a43343c841ee158db60a6454ff32
Jungmin Lee <lcalmsky@gmail.com>
2020-04-20 10:03:10 +0900

    Solve the problem
    - Runtime: 1 ms (81.12%)
    - Memory Usage: 52.4 MB (5.41%)

---
+++ src/test/java/io/lcalmsky/leetcode/kth_smallest_element_in_a_sorted_matrix/KthSmallestElementInASortedMatrixTests.java
@@ -0,0 +1,24 @@
+package io.lcalmsky.leetcode.kth_smallest_element_in_a_sorted_matrix;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class KthSmallestElementInASortedMatrixTests {
+    @Test
+    public void givenSortedMatrix_whenFindKthSmallestElement_thenCorrect() {
+        assertAll(
+                () -> test(new int[][]{{1, 5, 9}, {10, 11, 13}, {12, 13, 15}}, 8, 13)
+        );
+    }
+
+    private void test(int[][] given, int k, int expected) {
+        // when
+        KthSmallestElementInASortedMatrix kthSmallestElementInASortedMatrix = new KthSmallestElementInASortedMatrix();
+        int actual = kthSmallestElementInASortedMatrix.kthSmallest(given, k);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
