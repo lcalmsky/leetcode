d346371a6038046ba5c6099445c3fb89aa0c0f50
Jungmin Lee <lcalmsky@gmail.com>
2020-04-23 13:41:50 +0900

    Solve the problem
    - Runtime: 7 ms (85.81%)
    - Memory Usage: 39.7 MB (15.00%)

---
+++ src/main/java/io/lcalmsky/leetcode/first_unique_character_in_a_string/FirstUniqueCharacterInAString.java
@@ -0,0 +1,29 @@
+package io.lcalmsky.leetcode.first_unique_character_in_a_string;
+
+/**
+ * <pre>
+ * Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+ *
+ * Examples:
+ *
+ * s = "leetcode"
+ * return 0.
+ *
+ * s = "loveleetcode",
+ * return 2.
+ * Note: You may assume the string contain only lowercase letters.
+ * </pre>
+ */
+public class FirstUniqueCharacterInAString {
+    public int firstUniqChar(String s) {
+        int[] alphabets = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            alphabets[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (alphabets[s.charAt(i) - 'a'] == 1) return i;
+        }
+
+        return -1;
+    }
+}
