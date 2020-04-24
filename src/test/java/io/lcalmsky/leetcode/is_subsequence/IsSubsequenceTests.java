eeded55128013394a0e8aa38ef72677d4d3b2e85
Jungmin Lee <lcalmsky@gmail.com>
2020-04-24 11:48:30 +0900

    Solve the problem
    - Runtime: 10 ms (22.72%)
    - Memory Usage: 42.7 MB (100.00%)

---
+++ src/test/java/io/lcalmsky/leetcode/is_subsequence/IsSubsequenceTests.java
@@ -0,0 +1,25 @@
+package io.lcalmsky.leetcode.is_subsequence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IsSubsequenceTests {
+    @Test
+    public void givenTwoStrings_whenFindOneIsSubsequenceOfAnother_thenCorrect() {
+        assertAll(
+                () -> test("abc", "ahbgdc", true),
+                () -> test("axc", "ahbgdc", false)
+        );
+    }
+
+    private void test(String s, String t, boolean expected) {
+        // when
+        IsSubsequence isSubsequence = new IsSubsequence();
+        boolean actual = isSubsequence.isSubsequence(s, t);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
