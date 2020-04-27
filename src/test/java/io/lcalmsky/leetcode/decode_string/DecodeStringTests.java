daa64b476c62559d71e78b33527faaa02e2d654d
Jungmin Lee <lcalmsky@gmail.com>
2020-04-27 09:56:11 +0900

    Solve the problem
    - Runtime: 5 ms (32.05%)
    - Memory Usage: 37.6 MB (5.68%)

---
+++ src/test/java/io/lcalmsky/leetcode/decode_string/DecodeStringTests.java
@@ -0,0 +1,26 @@
+package io.lcalmsky.leetcode.decode_string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DecodeStringTests {
+    @Test
+    public void givenString_whenDecode_thenCorrect() {
+        assertAll(
+                () -> test("3[a]2[bc]", "aaabcbc"),
+                () -> test("3[a2[c]]", "accaccacc"),
+                () -> test("2[abc]3[cd]ef", "abcabccdcdcdef")
+        );
+    }
+
+    private void test(String given, String expected) {
+        // when
+        DecodeString decodeString = new DecodeString();
+        String actual = decodeString.decodeString(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
