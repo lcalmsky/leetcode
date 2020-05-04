95859d908b551c08651b89362d1e8dda90b851c2
Jungmin Lee <lcalmsky@gmail.com>
2020-05-04 11:21:34 +0900

    Solve the problem
    - Runtime: 1 ms (59.33%)
    - Memory Usage: 38.2 MB (7.84%)

---
+++ src/test/java/io/lcalmsky/leetcode/evaluate_division/EvaluateDivisionTests.java
@@ -0,0 +1,39 @@
+package io.lcalmsky.leetcode.evaluate_division;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class EvaluateDivisionTests {
+    @Test
+    public void givenEquationsValuesQueries_whenFindAnswer_thenCorrect() {
+        assertAll(
+                () -> test(
+                        Arrays.asList(
+                                Arrays.asList("a", "b"),
+                                Arrays.asList("b", "c")),
+                        new double[]{2.0, 3.0},
+                        Arrays.asList(
+                                Arrays.asList("a", "c"),
+                                Arrays.asList("b", "a"),
+                                Arrays.asList("a", "e"),
+                                Arrays.asList("a", "a"),
+                                Arrays.asList("x", "x")
+                        ),
+                        new double[]{6.0, 0.5, -1.0, 1.0, -1.0})
+        );
+    }
+
+    private void test(List<List<String>> equations, double[] values, List<List<String>> queries, double[] expected) {
+        // when
+        EvaluateDivision evaluateDivision = new EvaluateDivision();
+        double[] actual = evaluateDivision.calcEquation(equations, values, queries);
+
+        // then
+        assertArrayEquals(expected, actual);
+    }
+}
