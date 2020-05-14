9aa2009f86e5a4e69e264e46b40ce85c7a069f7d
Jungmin Lee <lcalmsky@gmail.com>
2020-05-14 12:57:49 +0900

    Solve the problem
    - Runtime: 6 ms (78.24%)
    - Memory Usage: 40.6 MB (97.87%)

---
+++ src/test/java/io/lcalmsky/leetcode/queue_reconstruction_by_height/QueueReconstructionByHeightTests.java
@@ -0,0 +1,28 @@
+package io.lcalmsky.leetcode.queue_reconstruction_by_height;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class QueueReconstructionByHeightTests {
+
+    @Test
+    public void givenQueue_whenReconstruction_thenOrderedByHeightCorrectly() {
+        assertAll(
+                () -> test(
+                        new int[][]{{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}},
+                        new int[][]{{5, 0}, {7, 0}, {5, 2}, {6, 1}, {4, 4}, {7, 1}}
+                )
+        );
+    }
+
+    private void test(int[][] given, int[][] expected) {
+        // when
+        QueueReconstructionByHeight queueReconstructionByHeight = new QueueReconstructionByHeight();
+        int[][] actual = queueReconstructionByHeight.reconstructQueue(given);
+
+        // then
+        assertArrayEquals(expected, actual);
+    }
+}
