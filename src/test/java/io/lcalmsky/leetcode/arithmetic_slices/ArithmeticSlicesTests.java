c4be0db53b8ff2f306eb96c2158b1e3d2a37656a
이정민(Jungmin Lee)/Payment개발팀/11ST <jaime.lee@sk.com>
2020-05-18 21:06:12 +0900

    Solve the problem
    - Runtime: 0 ms (100.00%)
    - Memory Usage: 37.4 MB (100.00%)

---
+++ src/test/java/io/lcalmsky/leetcode/arithmetic_slices/ArithmeticSlicesTests.java
@@ -0,0 +1,24 @@
+package io.lcalmsky.leetcode.arithmetic_slices;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArithmeticSlicesTests {
+    @Test
+    public void givenArray_whenSplit_thenArithmeticSlices() {
+        assertAll(
+                () -> test(new int[]{1, 2, 3, 4}, 3)
+        );
+    }
+
+    private void test(int[] given, int expected) {
+        // when
+        ArithmeticSlices arithmeticSlices = new ArithmeticSlices();
+        int actual = arithmeticSlices.numberOfArithmeticSlices(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
