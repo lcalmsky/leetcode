a3f83af1626a5f0ee994c55030831e96ed2931a3
Jungmin Lee <lcalmsky@gmail.com>
2020-05-19 00:51:36 +0900

    Solve the problem
    - Runtime: 2 ms (96.12%)
    - Memory Usage: 39.1 MB (6.25%)

---
+++ src/main/java/io/lcalmsky/leetcode/add_strings/AddStrings.java
@@ -0,0 +1,24 @@
+package io.lcalmsky.leetcode.add_strings;
+
+public class AddStrings {
+    public String addStrings(String num1, String num2) {
+        int n = num1.length();
+        int m = num2.length();
+        StringBuilder result = new StringBuilder();
+        int carry = 0;
+        for (int i = 0; i < Math.max(n, m); i++) {
+            int c1 = n - 1 - i >= 0 ? num1.charAt(n - 1 - i) - '0' : 0;
+            int c2 = m - 1 - i >= 0 ? num2.charAt(m - 1 - i) - '0' : 0;
+            int c = c1 + c2 + carry;
+            if (c >= 10) {
+                c -= 10;
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+            result.append(c);
+        }
+        if (carry != 0) result.append(carry);
+        return result.reverse().toString();
+    }
+}
