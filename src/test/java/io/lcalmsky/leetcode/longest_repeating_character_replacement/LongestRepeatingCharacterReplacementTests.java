937be1ad835bb523b6d0c6068955f806fbe54686
Jungmin Lee <lcalmsky@gmail.com>
2020-05-25 00:13:44 +0900

    Solve the problem
    - Runtime: 3 ms (99.40%)
    - Memory Usage: 39.3 MB (10.00%)

---
+++ src/test/java/io/lcalmsky/leetcode/longest_repeating_character_replacement/LongestRepeatingCharacterReplacementTests.java
@@ -0,0 +1,25 @@
+package io.lcalmsky.leetcode.longest_repeating_character_replacement;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LongestRepeatingCharacterReplacementTests {
+    @Test
+    public void givenCharacters_whenReplace_thenFindMaximumValueOfRepeatingCount() {
+        assertAll(
+                () -> test("ABAB", 2, 4),
+                () -> test("AABABBA", 1, 4)
+        );
+    }
+
+    private void test(String s, int k, int expected) {
+        // when
+        LongestRepeatingCharacterReplacement longestRepeatingCharacterReplacement = new LongestRepeatingCharacterReplacement();
+        int actual = longestRepeatingCharacterReplacement.characterReplacement(s, k);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
