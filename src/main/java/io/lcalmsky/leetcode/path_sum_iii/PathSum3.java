e7af90704c48a230be17b6b8200c5e832d9794c3
Jungmin Lee <lcalmsky@gmail.com>
2020-05-29 08:28:22 +0900

    Solve the problem (faster way)
    - Runtime: 2 ms (100.00%)
    - Memory Usage: 39.2 MB (88.64%)

--- src/main/java/io/lcalmsky/leetcode/path_sum_iii/PathSum3.java
+++ src/main/java/io/lcalmsky/leetcode/path_sum_iii/PathSum3.java
@@ -2,6 +2,9 @@ package io.lcalmsky.leetcode.path_sum_iii;
 
 import io.lcalmsky.leetcode.TreeNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <pre>
  * You are given a binary tree in which each node contains an integer value.
@@ -33,14 +36,29 @@ import io.lcalmsky.leetcode.TreeNode;
  */
 public class PathSum3 {
     public int pathSum(TreeNode root, int sum) {
-        if (root == null) return 0;
-        return pathSequenceSum(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        return recursive(root, 0, sum, map);
     }
 
-    private int pathSequenceSum(TreeNode node, int sum) {
+    public int recursive(TreeNode node, int accumulateSum, int sum, Map<Integer, Integer> map) {
         if (node == null) return 0;
-        int count = (sum == node.val) ? 1 : 0;
-        count += pathSequenceSum(node.left, sum - node.val) + pathSequenceSum(node.right, sum - node.val);
+        accumulateSum += node.val;
+        int count = map.getOrDefault(accumulateSum - sum, 0);
+        map.put(accumulateSum, map.getOrDefault(accumulateSum, 0) + 1);
+        count += recursive(node.left, accumulateSum, sum, map) + recursive(node.right, accumulateSum, sum, map);
+        map.put(accumulateSum, map.get(accumulateSum) - 1);
         return count;
     }
 }
+//    public int pathSum(TreeNode root, int sum) {
+//        if (root == null) return 0;
+//        return pathSequenceSum(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+//    }
+//
+//    private int pathSequenceSum(TreeNode node, int sum) {
+//        if (node == null) return 0;
+//        int count = (sum == node.val) ? 1 : 0;
+//        count += pathSequenceSum(node.left, sum - node.val) + pathSequenceSum(node.right, sum - node.val);
+//        return count;
+//    }
