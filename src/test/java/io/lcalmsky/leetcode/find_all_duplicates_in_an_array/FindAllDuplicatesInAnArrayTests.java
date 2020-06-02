0878c5079e802635fd372b9e92bfbec3050530ff
Jungmin Lee <lcalmsky@gmail.com>
2020-06-02 14:26:11 +0900

    Solve the problem
    - Runtime: 6 ms (47.24%)
    - Memory Usage: 48.7 MB (36.11%)

---
+++ src/test/java/io/lcalmsky/leetcode/find_all_duplicates_in_an_array/FindAllDuplicatesInAnArrayTests.java
@@ -0,0 +1,27 @@
+package io.lcalmsky.leetcode.find_all_duplicates_in_an_array;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FindAllDuplicatesInAnArrayTests {
+    @Test
+    public void givenArray_whenFindDuplicates_thenCorrect() {
+        assertAll(
+                () -> test(new int[]{4, 3, 2, 7, 8, 2, 3, 1}, Arrays.asList(2, 3))
+        );
+    }
+
+    private void test(int[] given, List<Integer> expected) {
+        // when
+        FindAllDuplicatesInAnArray findAllDuplicatesInAnArray = new FindAllDuplicatesInAnArray();
+        List<Integer> actual = findAllDuplicatesInAnArray.findDuplicates(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
