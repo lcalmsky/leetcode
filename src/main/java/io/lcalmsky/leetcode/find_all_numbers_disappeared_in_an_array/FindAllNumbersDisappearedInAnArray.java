4a237640f6bf28dc8e18b4597aa5c27259043e6f
Jungmin Lee <lcalmsky@gmail.com>
2020-06-04 09:27:00 +0900

    Solve the problem (another solution)
    - slower speed but less memory
    - Runtime: 5 ms (82.41%)
    - Memory Usage: 48.5 MB (33.96%)

--- src/main/java/io/lcalmsky/leetcode/find_all_numbers_disappeared_in_an_array/FindAllNumbersDisappearedInAnArray.java
+++ src/main/java/io/lcalmsky/leetcode/find_all_numbers_disappeared_in_an_array/FindAllNumbersDisappearedInAnArray.java
@@ -23,9 +23,19 @@ import java.util.List;
 public class FindAllNumbersDisappearedInAnArray {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         List<Integer> result = new ArrayList<>();
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            int val = Math.abs(nums[i]) - 1;
+            if (nums[val] > 0) nums[val] = -nums[val];
+        }
+        for (int i = 0; i < length; i++) if (nums[i] > 0) result.add(i + 1);
+        return result;
+    }
+    /*public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
         boolean[] exist = new boolean[nums.length + 1];
         for (int num : nums) exist[num] = true;
         for (int i = 1; i < exist.length; i++) if (!exist[i]) result.add(i);
         return result;
-    }
+    }*/
 }
