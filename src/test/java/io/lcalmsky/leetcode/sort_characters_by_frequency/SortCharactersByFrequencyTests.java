f27b08a2691f30209fdb6357c49f43e407b97846
Jungmin Lee <lcalmsky@gmail.com>
2020-06-08 10:03:29 +0900

    Solve the problem
    - Runtime: 15 ms (54.33%)
    - Memory Usage: 40.5 MB (62.93%)

---
+++ src/test/java/io/lcalmsky/leetcode/sort_characters_by_frequency/SortCharactersByFrequencyTests.java
@@ -0,0 +1,28 @@
+package io.lcalmsky.leetcode.sort_characters_by_frequency;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SortCharactersByFrequencyTests {
+    @Test
+    public void givenString_whenSortByFrequency_thenCorrect() {
+        assertAll(
+                () -> test("tree", "eert"),
+                () -> test("cccaaa", "cccaaa", "aaaccc"),
+                () -> test("Aabb", "bbAa")
+        );
+    }
+
+    private void test(String given, String... expected) {
+        // when
+        SortCharactersByFrequency sortCharactersByFrequency = new SortCharactersByFrequency();
+        String actual = sortCharactersByFrequency.frequencySort(given);
+
+        // then
+        assertTrue(Arrays.asList(expected).contains(actual));
+    }
+}
