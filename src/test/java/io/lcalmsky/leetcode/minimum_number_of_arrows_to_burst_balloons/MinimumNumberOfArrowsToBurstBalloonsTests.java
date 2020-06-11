6c7a70c72b9e58db5cd77285b2b021848a79ca3c
Jungmin Lee <lcalmsky@gmail.com>
2020-06-12 07:54:25 +0900

    Solve the problem
    Runtime: 17 ms, faster than 39.92% of Java online submissions for Minimum Number of Arrows to Burst Balloons.
    Memory Usage: 47.1 MB, less than 62.59% of Java online submissions for Minimum Number of Arrows to Burst Balloons.

---
+++ src/test/java/io/lcalmsky/leetcode/minimum_number_of_arrows_to_burst_balloons/MinimumNumberOfArrowsToBurstBalloonsTests.java
@@ -0,0 +1,24 @@
+package io.lcalmsky.leetcode.minimum_number_of_arrows_to_burst_balloons;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MinimumNumberOfArrowsToBurstBalloonsTests {
+    @Test
+    public void givenBalloons_whenShotArrows_thenBurstsWithMinimumNumber() {
+        assertAll(
+                () -> test(new int[][]{{10, 16}, {2, 8}, {1, 6}, {7, 12}}, 2)
+        );
+    }
+
+    private void test(int[][] given, int expected) {
+        // when
+        MinimumNumberOfArrowsToBurstBalloons minimumNumberOfArrowsToBurstBalloons = new MinimumNumberOfArrowsToBurstBalloons();
+        int actual = minimumNumberOfArrowsToBurstBalloons.findMinArrowShots(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
