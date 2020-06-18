9464086c48b7ca80e5bed4d5c8e66c14eda61a7e
Jungmin Lee <lcalmsky@gmail.com>
2020-06-18 09:45:49 +0900

    Solve the problem
    - Runtime: 25 ms, faster than 45.83% of Java online submissions for LFU Cache.
    - Memory Usage: 54 MB, less than 32.14% of Java online submissions for LFU Cache.

---
+++ src/test/java/io/lcalmsky/leetcode/lfu_cache/LFUCacheTests.java
@@ -0,0 +1,22 @@
+package io.lcalmsky.leetcode.lfu_cache;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LFUCacheTests {
+    @Test
+    public void givenLFUCache_whenPutAndGet_thenWorkCorrectly() {
+        LFUCache lfuCache = new LFUCache(2);
+        lfuCache.put(1, 1);
+        lfuCache.put(2, 2);
+        assertEquals(1, lfuCache.get(1));
+        lfuCache.put(3, 3);
+        assertEquals(-1, lfuCache.get(2));
+        assertEquals(3, lfuCache.get(3));
+        lfuCache.put(4, 4);
+        assertEquals(-1, lfuCache.get(1));
+        assertEquals(3, lfuCache.get(3));
+        assertEquals(4, lfuCache.get(4));
+    }
+}
