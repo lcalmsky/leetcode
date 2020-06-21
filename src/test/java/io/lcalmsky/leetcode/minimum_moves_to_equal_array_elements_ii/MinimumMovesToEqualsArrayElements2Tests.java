7a699bb0cfbe4bca309ecce93a5fbbe66e933ba3
Jungmin Lee <lcalmsky@gmail.com>
2020-06-22 08:11:01 +0900

    Solve the problem
    - Runtime: 2 ms, faster than 97.67% of Java online submissions for Minimum Moves to Equal Array Elements II.
    - Memory Usage: 40.1 MB, less than 63.41% of Java online submissions for Minimum Moves to Equal Array Elements II.

---
+++ src/test/java/io/lcalmsky/leetcode/minimum_moves_to_equal_array_elements_ii/MinimumMovesToEqualsArrayElements2Tests.java
@@ -0,0 +1,24 @@
+package io.lcalmsky.leetcode.minimum_moves_to_equal_array_elements_ii;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MinimumMovesToEqualsArrayElements2Tests {
+    @Test
+    public void givenArray_whenFindMinimumMovesToEqualsArrayElements_thenCorrect() {
+        assertAll(
+                () -> test(new int[]{1, 2, 3}, 2)
+        );
+    }
+
+    private void test(int[] given, int expected) {
+        // when
+        MinimumMovesToEqualsArrayElements2 minimumMovesToEqualsArrayElements2 = new MinimumMovesToEqualsArrayElements2();
+        int actual = minimumMovesToEqualsArrayElements2.minMoves2(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
