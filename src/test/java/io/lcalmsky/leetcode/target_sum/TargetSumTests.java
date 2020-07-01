16383443ef3ed5a836ab3d7e3e1e0984a5937b08
Jungmin Lee <lcalmsky@gmail.com>
2020-07-02 08:35:32 +0900

    Solve the problem
    - Runtime: 490 ms, faster than 22.81% of Java online submissions for Target Sum.
    - Memory Usage: 37.4 MB, less than 69.27% of Java online submissions for Target Sum.

---
+++ src/test/java/io/lcalmsky/leetcode/target_sum/TargetSumTests.java
@@ -0,0 +1,24 @@
+package io.lcalmsky.leetcode.target_sum;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TargetSumTests {
+    @Test
+    public void givenArray_whenCountNumberOfWays_thenCorrect() {
+        assertAll(
+                () -> test(new int[]{1, 1, 1, 1, 1}, 3, 5)
+        );
+    }
+
+    private void test(int[] given, int target, int expected) {
+        // when
+        TargetSum targetSum = new TargetSum();
+        int actual = targetSum.findTargetSumWays(given, target);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
