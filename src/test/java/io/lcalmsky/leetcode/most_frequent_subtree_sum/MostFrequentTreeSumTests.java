f3d3159c39fd429446adfeb0cd07d7780bf2c67d
Jungmin Lee <lcalmsky@gmail.com>
2020-07-08 08:57:12 +0900

    Solve the problem
    - Runtime: 15 ms, faster than 7.17% of Java online submissions for Most Frequent Subtree Sum.
    - Memory Usage: 42.7 MB, less than 18.46% of Java online submissions for Most Frequent Subtree Sum.

---
+++ src/test/java/io/lcalmsky/leetcode/most_frequent_subtree_sum/MostFrequentTreeSumTests.java
@@ -0,0 +1,26 @@
+package io.lcalmsky.leetcode.most_frequent_subtree_sum;
+
+import io.lcalmsky.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class MostFrequentTreeSumTests {
+    @Test
+    public void givenTreeNode_whenFindFrequentSubtreeSum_thenCorrect() {
+        assertAll(
+                () -> test(TreeNode.of(5, 2, -3), new int[]{2, -3, 4}),
+                () -> test(TreeNode.of(5, 2, -5), new int[]{2})
+        );
+    }
+
+    private void test(TreeNode given, int[] expected) {
+        // when
+        MostFrequentTreeSum mostFrequentTreeSum = new MostFrequentTreeSum();
+        int[] actual = mostFrequentTreeSum.findFrequentTreeSum(given);
+
+        // then
+        assertArrayEquals(expected, actual);
+    }
+}
