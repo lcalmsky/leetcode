fb934caabbc96dd39f07b58f71e0db20c4e7e3b1
Jaime <lcalmsky@gmail.com>
2020-07-10 13:21:30 +0900

    Solve the problem
    - Runtime: 33 ms, faster than 76.67% of Java online submissions for Longest Palindromic Subsequence.
    - Memory Usage: 49.9 MB, less than 44.14% of Java online submissions for Longest Palindromic Subsequence.

---
+++ src/test/java/io/lcalmsky/leetcode/longest_palindromic_subsequence/LongestPalindromicSubsequenceTests.java
@@ -0,0 +1,25 @@
+package io.lcalmsky.leetcode.longest_palindromic_subsequence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LongestPalindromicSubsequenceTests {
+    @Test
+    public void givenString_whenFindLongestPalindrome_thenCorrect() {
+        assertAll(
+                () -> test("bbbab", 4),
+                () -> test("cbbd", 2)
+        );
+    }
+
+    private void test(String given, int expected) {
+        // when
+        LongestPalindromicSubsequence longestPalindromicSubsequence = new LongestPalindromicSubsequence();
+        int actual = longestPalindromicSubsequence.longestPalindromeSubseq(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
