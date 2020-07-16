1b1151c92f5fe4449823fb6b7a60ed9dc8f073c3
Jaime <lcalmsky@gmail.com>
2020-07-17 08:21:44 +0900

    Solve the problem
    - Runtime: 70 ms, faster than 55.60% of Java online submissions for Beautiful Arrangement.
    - Memory Usage: 36.1 MB, less than 74.07% of Java online submissions for Beautiful Arrangement.

---
+++ src/test/java/io/lcalmsky/leetcode/beautiful_arrangement/BeautifulArrangementTests.java
@@ -0,0 +1,24 @@
+package io.lcalmsky.leetcode.beautiful_arrangement;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BeautifulArrangementTests {
+    @Test
+    public void givenNumber_whenCountArrangement_thenCorrect() {
+        assertAll(
+                () -> test(2, 2)
+        );
+    }
+
+    private void test(int given, int expected) {
+        // when
+        BeautifulArrangement beautifulArrangement = new BeautifulArrangement();
+        int actual = beautifulArrangement.countArrangement(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
