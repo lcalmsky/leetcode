9237708e62a3387a5e42edec9be0beb5f485d2af
Jaime Lee <lcalmsky@gmail.com>
2020-08-03 16:49:15 +0900

    Solve the problem
    - Runtime: 9 ms, faster than 90.58% of Java online submissions for Brick Wall.
    - Memory Usage: 42.8 MB, less than 39.29% of Java online submissions for Brick Wall.

---
+++ src/main/java/io/lcalmsky/leetcode/brick_wall/BrickWall.java
@@ -0,0 +1,56 @@
+package io.lcalmsky.leetcode.brick_wall;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <pre>
+ * If your line go through the edge of a brick, then the brick is not considered as crossed. You need to find out how to draw the line to cross the least bricks and return the number of crossed bricks.
+ *
+ * You cannot draw a line just along one of the two vertical edges of the wall, in which case the line will obviously cross no bricks.
+ *
+ *
+ *
+ * Example:
+ *
+ * Input: [[1,2,2,1],
+ *         [3,1,2],
+ *         [1,3,2],
+ *         [2,4],
+ *         [3,1,2],
+ *         [1,3,1,1]]
+ *
+ * Output: 2
+ *
+ * Explanation:
+ *
+ * <img src="https://assets.leetcode.com/uploads/2018/10/12/brick_wall.png">
+ *
+ * Note:
+ *
+ * The width sum of bricks in different rows are the same and won't exceed INT_MAX.
+ * The number of bricks in each row is in range [1,10,000]. The height of wall is in range [1,10,000]. Total number of bricks of the wall won't exceed 20,000.
+ * </pre>
+ */
+public class BrickWall {
+    public int leastBricks(List<List<Integer>> wall) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (List<Integer> row : wall) {
+            int sum = 0;
+            for (int i = 0; i < row.size() - 1; i++) {
+                sum += row.get(i);
+                if (map.containsKey(sum)) {
+                    map.put(sum, map.get(sum) + 1);
+                } else {
+                    map.put(sum, 1);
+                }
+            }
+        }
+        int result = wall.size();
+        for (int key : map.keySet()) {
+            result = Math.min(result, wall.size() - map.get(key));
+        }
+        return result;
+    }
+}
