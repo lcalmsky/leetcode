75c8505b70fb5c207d541b6c37640d1c6796cfc4
Jaime Lee <lcalmsky@gmail.com>
2020-08-19 12:51:39 +0900

    Solve the problem
    - Runtime: 4 ms, faster than 88.34% of Java online submissions for Permutation in String.
    - Memory Usage: 39.1 MB, less than 98.26% of Java online submissions for Permutation in String.

---
+++ src/test/java/io/lcalmsky/leetcode/permutation_in_string/PermutationInStringTest.java
@@ -0,0 +1,26 @@
+package io.lcalmsky.leetcode.permutation_in_string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PermutationInStringTest {
+    @Test
+    public void givenString_whenFindPermutation_thenCorrect() {
+        assertAll(
+                () -> test("ab", "eidbaooo", true),
+                () -> test("ab", "eidboaoo", false)
+        );
+    }
+
+    private void test(String s1, String s2, boolean expected) {
+        // when
+        PermutationInString permutationInString = new PermutationInString();
+        boolean actual = permutationInString.checkInclusion(s1, s2);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+}
