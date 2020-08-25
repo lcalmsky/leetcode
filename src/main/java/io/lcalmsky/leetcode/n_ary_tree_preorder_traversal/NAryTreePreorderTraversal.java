13c4f13400cc9b9a296c551a46c92df664cae166
Jaime Lee <lcalmsky@gmail.com>
2020-08-25 12:01:59 +0900

    Solve the problem
    - Runtime: 1 ms, faster than 75.01% of Java online submissions for N-ary Tree Preorder Traversal.
    - Memory Usage: 39.7 MB, less than 97.44% of Java online submissions for N-ary Tree Preorder Traversal.

---
+++ src/main/java/io/lcalmsky/leetcode/n_ary_tree_preorder_traversal/NAryTreePreorderTraversal.java
@@ -0,0 +1,22 @@
+package io.lcalmsky.leetcode.n_ary_tree_preorder_traversal;
+
+import io.lcalmsky.leetcode.n_ary_tree_level_order_traversal.Node;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class NAryTreePreorderTraversal {
+    public List<Integer> preorder(Node root) {
+        if (root == null) return Collections.emptyList();
+        List<Integer> list = new ArrayList<>();
+        helper(root, list);
+        return list;
+    }
+
+    private void helper(Node root, List<Integer> list) {
+        if (root == null) return;
+        list.add(root.val);
+        for (Node child : root.children) helper(child, list);
+    }
+}
