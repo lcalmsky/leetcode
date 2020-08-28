7ef375b52dcfa977eb9d73cb8d0d34e01e023967
Jaime Lee <lcalmsky@gmail.com>
2020-08-29 01:33:46 +0900

    Solve the problem
    - Runtime: 1 ms, faster than 97.08% of Java online submissions for Can Place Flowers.
    - Memory Usage: 41.2 MB, less than 51.55% of Java online submissions for Can Place Flowers.

---
+++ src/test/java/io/lcalmsky/leetcode/can_place_flowers/CanPlaceFlowersTest.java
@@ -0,0 +1,22 @@
+package io.lcalmsky.leetcode.can_place_flowers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CanPlaceFlowersTest {
+    @Test
+    public void givenFlowerbedAndNumberOfNewFlowers_whenPlaceNewFlowerWithoutAdjacent_thenCorrect() {
+        // then
+        assertAll(
+                () -> assertTrue(test(new int[]{1, 0, 0, 0, 1}, 1)),
+                () -> assertFalse(test(new int[]{1, 0, 0, 0, 1}, 2))
+        );
+    }
+
+    private boolean test(int[] given, int n) {
+        // when
+        CanPlaceFlowers canPlaceFlowers = new CanPlaceFlowers();
+        return canPlaceFlowers.canPlaceFlowers(given, n);
+    }
+}
