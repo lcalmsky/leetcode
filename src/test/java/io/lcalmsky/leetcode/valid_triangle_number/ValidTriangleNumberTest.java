9b02c434e0d15a86e4dacc6a61404c62cfd9d0ca
Jaime Lee <lcalmsky@gmail.com>
2020-08-30 01:06:23 +0900

    Solve the problem
    - Runtime: 100 ms, faster than 25.54% of Java online submissions for Valid Triangle Number.
    - Memory Usage: 40.1 MB, less than 22.92% of Java online submissions for Valid Triangle Number.

---
+++ src/test/java/io/lcalmsky/leetcode/valid_triangle_number/ValidTriangleNumberTest.java
@@ -0,0 +1,24 @@
+package io.lcalmsky.leetcode.valid_triangle_number;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ValidTriangleNumberTest {
+    @Test
+    public void givenIntegers_whenMakeTriangles_thenFindValidLengths() {
+        assertAll(
+                () -> test(new int[]{2, 2, 3, 4}, 3)
+        );
+    }
+
+    private void test(int[] given, int expected) {
+        // when
+        ValidTriangleNumber validTriangleNumber = new ValidTriangleNumber();
+        int actual = validTriangleNumber.triangleNumber(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
