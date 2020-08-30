e5fb8058cb1c94811fc81bff065d06319b952ee7
Jaime Lee <lcalmsky@gmail.com>
2020-08-31 00:31:59 +0900

    Solve the problem
    - Runtime: 1 ms, faster than 57.67% of Java online submissions for Merge Two Binary Trees.
    - Memory Usage: 48.6 MB, less than 33.98% of Java online submissions for Merge Two Binary Trees.

---
+++ src/test/java/io/lcalmsky/leetcode/merge_two_binary_trees/MergeTwoBinaryTreesTest.java
@@ -0,0 +1,26 @@
+package io.lcalmsky.leetcode.merge_two_binary_trees;
+
+import io.lcalmsky.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MergeTwoBinaryTreesTest {
+    @Test
+    public void givenTwoBinaryTree_whenMergeThem_thenCorrect() {
+        assertAll(
+                () -> test(TreeNode.of(1, 3, 2, 5), TreeNode.of(2, 1, 3, null, 4, 7), TreeNode.of(3, 4, 5, 5, 4, 7))
+        );
+    }
+
+    private void test(TreeNode t1, TreeNode t2, TreeNode expected) {
+        // when
+        MergeTwoBinaryTrees mergeTwoBinaryTrees = new MergeTwoBinaryTrees();
+        TreeNode actual = mergeTwoBinaryTrees.mergeTrees(t1, t2);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+}
