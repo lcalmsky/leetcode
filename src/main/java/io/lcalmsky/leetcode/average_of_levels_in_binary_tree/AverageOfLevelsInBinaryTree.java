7e9b6be5181fa196c26708d30a171327ae0856f7
Jaime Lee <lcalmsky@gmail.com>
2020-09-09 16:47:08 +0900

    Solve the problem
    - Runtime: 2 ms, faster than 83.09% of Java online submissions for Average of Levels in Binary Tree.
    - Memory Usage: 41.2 MB, less than 80.68% of Java online submissions for Average of Levels in Binary Tree.

---
+++ src/main/java/io/lcalmsky/leetcode/average_of_levels_in_binary_tree/AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,49 @@
+package io.lcalmsky.leetcode.average_of_levels_in_binary_tree;
+
+import io.lcalmsky.leetcode.TreeNode;
+
+import java.util.*;
+
+/**
+ * <pre>
+ * Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+ *
+ * Example 1:
+ * Input:
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * Output: [3, 14.5, 11]
+ * Explanation:
+ * The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
+ *
+ * Note:
+ * The range of node's value is in the range of 32-bit signed integer.
+ * </pre>
+ */
+public class AverageOfLevelsInBinaryTree {
+    public List<Double> averageOfLevels(TreeNode root) {
+        if (root == null) return Collections.emptyList();
+        List<Double> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(sum / size);
+        }
+        return result;
+    }
+}
