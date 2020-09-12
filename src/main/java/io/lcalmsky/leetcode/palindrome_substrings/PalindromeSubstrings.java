8962ce44b819343770f2a60953a061012b6319d5
Jaime Lee <lcalmsky@gmail.com>
2020-09-13 01:04:51 +0900

    Solve the problem
    - Runtime: 27 ms, faster than 16.23% of Java online submissions for Palindromic Substrings.
    - Memory Usage: 41.4 MB, less than 16.33% of Java online submissions for Palindromic Substrings.

---
+++ src/main/java/io/lcalmsky/leetcode/palindrome_substrings/PalindromeSubstrings.java
@@ -0,0 +1,40 @@
+package io.lcalmsky.leetcode.palindrome_substrings;
+
+/**
+ * <pre>
+ * Given a string, your task is to count how many palindromic substrings in this string.
+ *
+ * The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+ *
+ * Example 1:
+ *
+ * Input: "abc"
+ * Output: 3
+ * Explanation: Three palindromic strings: "a", "b", "c".
+ *
+ *
+ * Example 2:
+ *
+ * Input: "aaa"
+ * Output: 6
+ * Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ *
+ *
+ * Note:
+ *
+ * The input string length won't exceed 1000.
+ * </pre>
+ */
+public class PalindromeSubstrings {
+    public int countSubstrings(String s) {
+        int n = s.length(), res = 0;
+        boolean[][] dp = new boolean[n][n];
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i; j < n; ++j) {
+                dp[i][j] = (s.charAt(i) == s.charAt(j)) && (j - i <= 2 || dp[i + 1][j - 1]);
+                if (dp[i][j]) ++res;
+            }
+        }
+        return res;
+    }
+}
