69e8d20ced026d649b94ad90e2cd0a61a0b00e16
Jaime Lee <lcalmsky@gmail.com>
2020-09-14 16:33:26 +0900

    Solve the problem
    - Runtime: 272 ms, faster than 6.57% of Java online submissions for Replace Words.
    - Memory Usage: 118.8 MB, less than 7.92% of Java online submissions for Replace Words.

---
+++ src/test/java/io/lcalmsky/leetcode/replace_words/ReplaceWordsTest.java
@@ -0,0 +1,46 @@
+package io.lcalmsky.leetcode.replace_words;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ReplaceWordsTest {
+    @Test
+    public void givenDictionary_whenReplaceWords_thenCorrect() {
+        assertAll(
+                () -> test(
+                        Arrays.asList("cat", "bat", "rat"),
+                        "the cattle was rattled by the battery",
+                        "the cat was rat by the bat"),
+                () -> test(
+                        Arrays.asList("a", "b", "c"),
+                        "aadsfasf absbs bbab cadsfafs",
+                        "a a b c"),
+                () -> test(
+                        Arrays.asList("a", "aa", "aaa", "aaaa"),
+                        "a aa a aaaa aaa aaa aaa aaaaaa bbb baba ababa",
+                        "a a a a a a a a bbb baba a"),
+                () -> test(
+                        Arrays.asList("catt", "cat", "bat", "rat"),
+                        "the cattle was rattled by the battery",
+                        "the cat was rat by the bat"),
+                () -> test(
+                        Arrays.asList("ac", "ab"),
+                        "it is abnormal that this solution is accepted",
+                        "it is ab that this solution is ac")
+        );
+    }
+
+    private void test(List<String> dictionary, String sentence, String expected) {
+        // when
+        ReplaceWords replaceWords = new ReplaceWords();
+        String actual = replaceWords.replaceWords(dictionary, sentence);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
