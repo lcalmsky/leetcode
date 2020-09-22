7e652c718758b6d7ca63d8a57a84acd0a52961fe
Jaime Lee <lcalmsky@gmail.com>
2020-09-22 11:21:02 +0900

    Solve the problem
    - Runtime: 12 ms, faster than 10.92% of Java online submissions for Robot Return to Origin.
    - Memory Usage: 40.3 MB, less than 30.13% of Java online submissions for Robot Return to Origin.

---
+++ src/test/java/io/lcalmsky/leetcode/robot_return_to_origin/RobotReturnToOriginTest.java
@@ -0,0 +1,27 @@
+package io.lcalmsky.leetcode.robot_return_to_origin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RobotReturnToOriginTest {
+    @Test
+    public void givenString_whenMakeSureTheRobotReturnsToItsPlace_thenCorrect() {
+        assertAll(
+                () -> test("UD", true),
+                () -> test("LL", false),
+                () -> test("RRDD", false),
+                () -> test("LDRRLRUULR", false)
+        );
+    }
+
+    private void test(String given, boolean expected) {
+        // when
+        RobotReturnToOrigin robotReturnToOrigin = new RobotReturnToOrigin();
+        boolean actual = robotReturnToOrigin.judgeCircle(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
