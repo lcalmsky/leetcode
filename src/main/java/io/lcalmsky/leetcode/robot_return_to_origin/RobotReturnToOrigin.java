d15babba842b8200e3bbbc96d1b99f7b78a355b4
Jaime Lee <lcalmsky@gmail.com>
2020-09-22 11:22:54 +0900

    Solve the problem (faster solution)
    - Runtime: 6 ms, faster than 50.67% of Java online submissions for Robot Return to Origin.
    - Memory Usage: 39.3 MB, less than 82.84% of Java online submissions for Robot Return to Origin.

--- src/main/java/io/lcalmsky/leetcode/robot_return_to_origin/RobotReturnToOrigin.java
+++ src/main/java/io/lcalmsky/leetcode/robot_return_to_origin/RobotReturnToOrigin.java
@@ -53,3 +53,19 @@ public class RobotReturnToOrigin {
                 && map.getOrDefault('U', 0).equals(map.getOrDefault('D', 0));
     }
 }
+
+class AnotherSolution {
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < moves.length(); i++) {
+            if (moves.charAt(i) == 'U') y++;
+            else if (moves.charAt(i) == 'D') y--;
+            else if (moves.charAt(i) == 'R') x++;
+            else x--;
+        }
+
+        return x == 0 && y == 0;
+    }
+}
