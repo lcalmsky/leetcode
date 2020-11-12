41d74fa90f566f780f93eee17fcb3417b777320b
Jaime <lcalmsky@gmail.com>
2020-11-12 11:57:43 +0900

    Solve the problem
    - Runtime: 2 ms, faster than 99.50% of Java online submissions for Max Area of Island.
    - Memory Usage: 39.9 MB, less than 8.24% of Java online submissions for Max Area of Island.

---
+++ src/test/java/io/lcalmsky/leetcode/max_area_of_island/MaxAreaOfIslandTest.java
@@ -0,0 +1,36 @@
+package io.lcalmsky.leetcode.max_area_of_island;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MaxAreaOfIslandTest {
+    @Test
+    public void given2DArray_whenFindMaximumAreaOfIsland_thenCorrect() {
+        assertAll(
+                () -> test(new int[][]{
+                        {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                        {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                        {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+                }, 6),
+                () -> test(new int[][]{
+                        {0, 0, 0, 0, 0, 0, 0, 0}
+                }, 0)
+        );
+    }
+
+    private void test(int[][] given, int expected) {
+        // when
+        MaxAreaOfIsland maxAreaOfIsland = new MaxAreaOfIsland();
+        int actual = maxAreaOfIsland.maxAreaOfIsland(given);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
