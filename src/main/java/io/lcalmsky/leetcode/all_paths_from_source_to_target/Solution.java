3c67b82ff6e425ccab44982de7b67f76303223d7
Jaime <lcalmsky@gmail.com>
2021-03-04 11:55:21 +0900

    Solve the problem
    - Runtime: 4 ms, faster than 35.04% of Java online submissions for All Paths From Source to Target.
    - Memory Usage: 42.6 MB, less than 22.21% of Java online submissions for All Paths From Source to Target.

---
+++ src/main/java/io/lcalmsky/leetcode/all_paths_from_source_to_target/Solution.java
@@ -0,0 +1,27 @@
+package io.lcalmsky.leetcode.all_paths_from_source_to_target;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> result = new LinkedList<>();
+        List<Integer> path = new LinkedList<>();
+        path.add(0);
+        dfs(graph, 0, path, result);
+        return result;
+    }
+
+    private void dfs(int[][] graph, int index, List<Integer> path, List<List<Integer>> rst) {
+        if (index == graph.length - 1) {
+            rst.add(new LinkedList<>(path));
+            return;
+        }
+
+        for (int neighbor : graph[index]) {
+            path.add(neighbor);
+            dfs(graph, neighbor, path, rst);
+            path.remove(path.size() - 1);
+        }
+    }
+}
