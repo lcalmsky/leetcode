cb63e6d21b22c079fdcd15f1d25b38528a9f203a
Jaime <lcalmsky@gmail.com>
2021-03-26 09:25:57 +0900

    Solve the problem
    - Runtime: 0 ms, faster than 100.00% of Java online submissions for Score of Parentheses.
    - Memory Usage: 36.7 MB, less than 97.54% of Java online submissions for Score of Parentheses.

--- src/main/java/io/lcalmsky/leetcode/score_of_parentheses/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/score_of_parentheses/Solution.java
@@ -4,16 +4,17 @@ import java.util.Stack;
 
 public class Solution {
     public int scoreOfParentheses(String S) {
-        Stack<Integer> parentheses = new Stack<>();
-        int multiplier = 0;
-        for (char c : S.toCharArray()) {
-            if (c == '(') {
-                parentheses.push(multiplier);
-                multiplier = 0;
-            } else {
-                multiplier = parentheses.pop() + Math.max(2 * multiplier, 1);
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == '(') stack.push(0);
+            else {
+                int x = stack.pop();
+                int y = stack.pop();
+                stack.push(y + Math.max(2 * x, 1));
             }
         }
-        return multiplier;
+        return stack.pop();
     }
 }
