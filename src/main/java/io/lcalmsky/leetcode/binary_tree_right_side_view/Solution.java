cbe57bfe5f41cc00da08b4d3411fea662ec3effe
Jaime <lcalmsky@gmail.com>
2021-08-23 08:26:07 +0900

    Solve the problem

--- src/main/java/io/lcalmsky/leetcode/binary_tree_right_side_view/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/binary_tree_right_side_view/Solution.java
@@ -26,4 +26,24 @@ public class Solution {
 
         return values;
     }
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.left != null) {
+            if (root.left.val >= root.val) {
+                return false;
+            }
+        }
+
+        if (root.right != null) {
+            if (root.right.val <= root.val) {
+                return false;
+            }
+        }
+
+        return isValidBST(root.left) && isValidBST(root.right);
+    }
 }
