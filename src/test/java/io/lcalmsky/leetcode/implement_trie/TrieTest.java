4ac7d4a8f4e9c531d6ea58a1ce7cf2fd94bb05a5
Jaime <lcalmsky@gmail.com>
2021-10-09 03:40:59 +0900

    Solved

---
+++ src/test/java/io/lcalmsky/leetcode/implement_trie/TrieTest.java
@@ -0,0 +1,19 @@
+package io.lcalmsky.leetcode.implement_trie;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TrieTest {
+    @Test
+    void test() {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        assertTrue(trie.search("apple"));
+        assertFalse(trie.search("app"));
+        assertTrue(trie.startsWith("app"));
+        trie.insert("app");
+        assertTrue(trie.search("app"));
+    }
+}
