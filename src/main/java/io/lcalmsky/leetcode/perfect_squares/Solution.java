25547b3ddb9d72d5f74edbe3c09378dc3b734dd7
Jaime <lcalmsky@gmail.com>
2021-10-14 13:57:40 +0900

    Solve

--- src/main/java/io/lcalmsky/leetcode/perfect_squares/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/perfect_squares/Solution.java
@@ -4,15 +4,46 @@ import java.util.Arrays;
 
 public class Solution {
     public int numSquares(int n) {
-        int max = (int) Math.sqrt(n);
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        Arrays.fill(dp, 10001);
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= max; j++) {
-                if (i == j * j) dp[i] = 1;
-                else if (i > j * j) dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            for (int j = 1; j <= Math.sqrt(n); j++) {
+                if (i == j * j) {
+                    dp[i] = 1;
+                } else if (i > j * j) {
+                    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+                }
             }
         }
         return dp[n];
+    }
+}
+
+class AnotherSolution extends Solution {
+    public int numSquares(int n) {
+        while (n % 4 == 0) {
+            n /= 4;
+        }
+
+        if (n % 8 == 7) {
+            return 4;
+        }
+
+        if (isSquares(n)) {
+            return 1;
+        }
+
+        for (int i = 1; i * i < n; i++) {
+            if (isSquares(n - i * i)) {
+                return 2;
+            }
+        }
+
+        return 3;
+    }
+
+    private boolean isSquares(int n) {
+        int rootN = (int) Math.sqrt(n);
+        return n == rootN * rootN;
     }
 }
--- src/test/java/io/lcalmsky/leetcode/perfect_squares/SolutionTest.java
+++ src/test/java/io/lcalmsky/leetcode/perfect_squares/SolutionTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PerfectSquaresTests {
+class SolutionTest {
     @Test
-    public void givenNumber_whenFindLeastNumber_thenCorrect() {
+    void givenNumber_whenFindLeastNumber_thenCorrect() {
         assertAll(
                 () -> test(12, 3),
                 () -> test(13, 2)
@@ -16,8 +16,8 @@ public class PerfectSquaresTests {
 
     private void test(int given, int expected) {
         // when
-        Solution perfectSquares = new Solution();
-        int actual = perfectSquares.numSquares(given);
+        Solution solution = new Solution();
+        int actual = solution.numSquares(given);
 
         // then
         assertEquals(actual, expected);
