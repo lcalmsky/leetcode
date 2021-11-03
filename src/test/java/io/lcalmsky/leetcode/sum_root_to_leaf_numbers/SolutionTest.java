73fa4b837cc9875a8431e2670c7c7feb2a7ae16f
Jaime <lcalmsky@gmail.com>
2021-11-03 17:58:48 +0900

    Add README.md #11

--- src/test/java/io/lcalmsky/leetcode/sum_root_to_leaf_numbers/SolutionTest.java
+++ src/test/java/io/lcalmsky/leetcode/sum_root_to_leaf_numbers/SolutionTest.java
@@ -1,17 +1,18 @@
 package io.lcalmsky.leetcode.sum_root_to_leaf_numbers;
 
-import io.lcalmsky.leetcode.TreeNode;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SumRootToLeafNumbersTests {
+import io.lcalmsky.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
     @Test
-    public void givenTreeNode_whenSumRootToLeaf_thenCorrect() {
+    void givenTreeNode_whenSumRootToLeaf_thenCorrect() {
         assertAll(
-                () -> test(TreeNode.of(1, 2, 3), 25),
-                () -> test(TreeNode.of(4, 9, 0, 5, 1), 1026)
+            () -> test(TreeNode.of(1, 2, 3), 25),
+            () -> test(TreeNode.of(4, 9, 0, 5, 1), 1026)
         );
     }
 
@@ -23,5 +24,4 @@ public class SumRootToLeafNumbersTests {
         // then
         assertEquals(expected, actual);
     }
-
 }
