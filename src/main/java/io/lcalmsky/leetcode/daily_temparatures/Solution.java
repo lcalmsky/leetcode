7a9bf01d9e739fc57d508c9ddf449282dc6d6cf9
Jaime (Jungmin Lee) <lcalmsky@gmail.com>
2021-11-13 18:33:09 +0900

    Solve the problem: resolve #24 (#25)

--- src/main/java/io/lcalmsky/leetcode/daily_temparatures/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/daily_temparatures/Solution.java
@@ -2,20 +2,41 @@ package io.lcalmsky.leetcode.daily_temparatures;
 
 import java.util.Stack;
 
+/**
+ * Runtime: 32 ms, faster than 73.20% of Java online submissions for Daily Temperatures.
+ * Memory Usage: 48.7 MB, less than 68.59% of Java online submissions for Daily Temperatures.
+ */
 public class Solution {
-    public int[] dailyTemperatures(int[] T) {
-        int[] result = new int[T.length];
-        if (T.length == 0) return result;
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        if (temperatures.length == 0) return result;
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < T.length; ++i) {
-            while (!stack.empty() && T[i] > T[stack.peek()]) {
+        for (int i = 0; i < temperatures.length; ++i) {
+            while (!stack.empty() && temperatures[i] > temperatures[stack.peek()]) {
                 int index = stack.pop();
                 result[index] = i - index;
             }
             stack.push(i);
         }
-        while (!stack.empty()) {
-            result[stack.pop()] = 0;
+        return result;
+    }
+}
+
+/**
+ * Runtime: 7 ms, faster than 97.35% of Java online submissions for Daily Temperatures.
+ * Memory Usage: 54.2 MB, less than 38.05% of Java online submissions for Daily Temperatures.
+ */
+class Solution2 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        int[] stack = new int[temperatures.length];
+        int top = -1;
+        for(int i = 0; i < temperatures.length; i++) {
+            while(top > -1 && temperatures[i] > temperatures[stack[top]]) {
+                int idx = stack[top--];
+                result[idx] = i - idx;
+            }
+            stack[++top] = i;
         }
         return result;
     }
--- src/test/java/io/lcalmsky/leetcode/daily_temparatures/SolutionTest.java
+++ src/test/java/io/lcalmsky/leetcode/daily_temparatures/SolutionTest.java
@@ -1,24 +1,27 @@
 package io.lcalmsky.leetcode.daily_temparatures;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import org.junit.jupiter.api.Test;
+
 class SolutionTest {
-    @Test
-    public void givenTemperatures_whenCountDaysUntilWarmerTemperature_thenCorrect() {
-        assertAll(
-                () -> test(new int[]{73, 74, 75, 71, 69, 72, 76, 73}, new int[]{1, 1, 4, 2, 1, 1, 0, 0})
-        );
-    }
 
-    private void test(int[] given, int[] expected) {
-        // when
-        Solution solution = new Solution();
-        int[] actual = solution.dailyTemperatures(given);
+  @Test
+  void givenTemperatures_whenCountDaysUntilWarmerTemperature_thenCorrect() {
+    assertAll(
+        () -> test(new int[]{73, 74, 75, 71, 69, 72, 76, 73}, new int[]{1, 1, 4, 2, 1, 1, 0, 0}),
+        () -> test(new int[]{30, 40, 50, 60}, new int[]{1, 1, 1, 0}),
+        () -> test(new int[]{30, 60, 90}, new int[]{1, 1, 0})
+    );
+  }
 
-        // then
-        assertArrayEquals(expected, actual);
-    }
+  private void test(int[] given, int[] expected) {
+    // when
+    Solution solution = new Solution();
+    int[] actual = solution.dailyTemperatures(given);
+
+    // then
+    assertArrayEquals(expected, actual);
+  }
 }
