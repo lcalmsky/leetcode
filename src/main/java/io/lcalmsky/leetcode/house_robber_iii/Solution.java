ea226fee4392b80bd0d9b606c9bf8b936553bb38
Jaime (Jungmin Lee) <lcalmsky@gmail.com>
2021-12-06 09:12:56 +0900

    Solve the problem: resolve #51 (#52)

--- src/main/java/io/lcalmsky/leetcode/house_robber_iii/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/house_robber_iii/Solution.java
@@ -35,26 +35,24 @@ import io.lcalmsky.leetcode.TreeNode;
  * </pre>
  */
 public class Solution {
-    public int rob(TreeNode root) {
-        if (root == null)
-            return 0;
 
-        int[] result = helper(root);
-        return Math.max(result[0], result[1]);
+  public int rob(TreeNode root) {
+    if (root == null) {
+      return 0;
     }
+    int[] result = helper(root);
+    return Math.max(result[0], result[1]);
+  }
 
-    public int[] helper(TreeNode root) {
-        if (root == null) {
-            return new int[]{0, 0};
-        }
-
-        int[] result = new int[2];
-        int[] left = helper(root.left);
-        int[] right = helper(root.right);
-
-        result[0] = root.val + left[1] + right[1];
-        result[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
-
-        return result;
+  public int[] helper(TreeNode root) {
+    if (root == null) {
+      return new int[]{0, 0};
     }
+    int[] result = new int[2];
+    int[] left = helper(root.left);
+    int[] right = helper(root.right);
+    result[0] = root.val + left[1] + right[1];
+    result[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+    return result;
+  }
 }
