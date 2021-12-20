2b7ab6cf3469378fceecc0437a16e9a28721da5c
Jaime <lcalmsky@gmail.com>
2021-12-21 08:26:16 +0900

    Solve the problem: #80

--- src/test/java/io/lcalmsky/leetcode/minimum_absolute_difference/SolutionTest.java
+++ src/test/java/io/lcalmsky/leetcode/minimum_absolute_difference/SolutionTest.java
@@ -1,11 +1,13 @@
 package io.lcalmsky.leetcode.minimum_absolute_difference;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class SolutionTest {
+
   @Test
   void testAll() {
     assertAll(
@@ -13,13 +15,21 @@ class SolutionTest {
             List.of(1, 2),
             List.of(2, 3),
             List.of(3, 4)
+        )),
+        () -> test(new int[]{1, 3, 6, 10, 15}, List.of(
+            List.of(1, 3)
+        )),
+        () -> test(new int[]{3, 8, -10, 23, 19, -4, -14, 27}, List.of(
+            List.of(-14, -10),
+            List.of(19, 23),
+            List.of(23, 27)
         ))
     );
   }
 
   private void test(int[] given, List<List<Integer>> expected) {
     // when
-    Solution solution = new Solution();;
+    Solution solution = new Solution();
     List<List<Integer>> actual = solution.minimumAbsDifference(given);
     // then
     assertEquals(expected, actual);
