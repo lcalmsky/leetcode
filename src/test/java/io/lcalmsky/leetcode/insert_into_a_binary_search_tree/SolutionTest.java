ad37031916900c3602e7fc3442989434110a9e3a
Jaime <lcalmsky@gmail.com>
2022-01-12 09:51:45 +0900

    Solve the problem: #122

--- src/test/java/io/lcalmsky/leetcode/insert_into_a_binary_search_tree/SolutionTest.java
+++ src/test/java/io/lcalmsky/leetcode/insert_into_a_binary_search_tree/SolutionTest.java
@@ -1,28 +1,31 @@
 package io.lcalmsky.leetcode.insert_into_a_binary_search_tree;
 
-import io.lcalmsky.leetcode.TreeNode;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import io.lcalmsky.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
 class SolutionTest {
-    @Test
-    public void givenBinarySearchTree_whenInsertTreeNode_thenCorrect() {
-        assertAll(
-                () -> test(null, 5, TreeNode.of(5)),
-                () -> test(TreeNode.of(4, 2, 7, 1, 3), 5, TreeNode.of(4, 2, 7, 1, 3, 5)),
-                () -> test(TreeNode.of(40, 20, 60, 10, 30, 50, 70), 25, TreeNode.of(40, 20, 60, 10, 30, 50, 70, null, null, 25)),
-                () -> test(TreeNode.of(4, 2, 7, 1, 3, null, null, null, null, null, null), 5, TreeNode.of(4, 2, 7, 1, 3, 5))
-        );
-    }
 
-    private void test(TreeNode root, int k, TreeNode expected) {
-        // when
-        Solution solution = new Solution();
-        TreeNode actual = solution.insertIntoBST(root, k);
+  @Test
+  void givenBinarySearchTree_whenInsertTreeNode_thenCorrect() {
+    assertAll(
+        () -> test(null, 5, TreeNode.of(5)),
+        () -> test(TreeNode.of(4, 2, 7, 1, 3), 5, TreeNode.of(4, 2, 7, 1, 3, 5)),
+        () -> test(TreeNode.of(40, 20, 60, 10, 30, 50, 70), 25,
+            TreeNode.of(40, 20, 60, 10, 30, 50, 70, null, null, 25)),
+        () -> test(TreeNode.of(4, 2, 7, 1, 3, null, null, null, null, null, null), 5,
+            TreeNode.of(4, 2, 7, 1, 3, 5))
+    );
+  }
 
-        // then
-        assertEquals(expected, actual);
-    }
+  private void test(TreeNode root, int k, TreeNode expected) {
+    // when
+    Solution solution = new Solution();
+    TreeNode actual = solution.insertIntoBST(root, k);
+
+    // then
+    assertEquals(expected, actual);
+  }
 }
