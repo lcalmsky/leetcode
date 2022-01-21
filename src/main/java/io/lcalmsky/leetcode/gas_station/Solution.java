81b518a57ba63b811c0057bc6cb74f8364b11b3e
이정민(Jungmin Lee)/Payment개발팀/11ST <jaime.lee@sk.com>
2022-01-21 10:11:01 +0900

    Solve the problem: #138

--- src/main/java/io/lcalmsky/leetcode/gas_station/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/gas_station/Solution.java
@@ -1,18 +1,18 @@
 package io.lcalmsky.leetcode.gas_station;
 
 public class Solution {
-    public int canCompleteCircuit(int[] gas, int[] cost) {
-        int sum = 0, total = 0, start = 0, remaining;
-        for (int i = 0; i < gas.length; i++) {
-            remaining = gas[i] - cost[i];
-            if (sum >= 0) {
-                sum += remaining;
-            } else {
-                sum = remaining;
-                start = i;
-            }
-            total += remaining;
-        }
-        return total >= 0 ? start : -1;
+
+  public int canCompleteCircuit(int[] gas, int[] cost) {
+    int total = 0, start = 0, remaining = 0;
+    for (int i = 0; i < gas.length; i++) {
+      int diff = gas[i] - cost[i];
+      remaining += diff; // (1)
+      total += diff; // (2)
+      if (remaining < 0) { // (3)
+        remaining = 0;
+        start = i + 1;
+      }
     }
+    return total >= 0 ? start : -1; // (4)
+  }
 }
