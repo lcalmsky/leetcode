c47df94e138865f70da912275b4b045185e553f4
Jaime <lcalmsky@gmail.com>
2022-01-27 11:00:27 +0900

    Solve the problem: #150

--- src/main/java/io/lcalmsky/leetcode/maximum_xor_of_two_numbers_in_an_array/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/maximum_xor_of_two_numbers_in_an_array/Solution.java
@@ -3,43 +3,28 @@ package io.lcalmsky.leetcode.maximum_xor_of_two_numbers_in_an_array;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * <pre>
- * Given a non-empty array of numbers, a0, a1, a2, … , an-1, where 0 ≤ ai < 231.
- *
- * Find the maximum result of ai XOR aj, where 0 ≤ i, j < n.
- *
- * Could you do this in O(n) runtime?
- *
- * Example:
- *
- * Input: [3, 10, 5, 25, 2, 8]
- *
- * Output: 28
- *
- * Explanation: The maximum result is 5 ^ 25 = 28.
- * </pre>
- */
 public class Solution {
-    public int findMaximumXOR(int[] nums) {
-        int result = 0, mask = 0;
-        for (int i = 31; i >= 0; i--) {
-            // Prepare prefixes
-            mask = mask | (1 << i);
-            final Set<Integer> set = new HashSet<>();
-            for (int num : nums) {
-                set.add(num & mask);
-            }
 
-            // Validate result ^ B = C?
-            int temp = result | (1 << i);
-            for (int prefix : set) {
-                if (set.contains(temp ^ prefix)) {
-                    result = temp;
-                    break;
-                }
-            }
-        }
-        return result;
+  public int findMaximumXOR(int[] nums) {
+    int max = 0, mask = 0;
+    Set<Integer> set = new HashSet<Integer>();
+    int maxNum = 0;
+    for (int num : nums) {
+      maxNum = Math.max(maxNum, num);
     }
+    for (int i = 31 - Integer.numberOfLeadingZeros(maxNum); i >= 0; i--) {
+      set.clear();
+      int bit = 1 << i;
+      mask = mask | bit;
+      int temp = max | bit;
+      for (int prefix : nums) {
+        if (set.contains((prefix & mask) ^ temp)) {
+          max = temp;
+          break;
+        }
+        set.add(prefix & mask);
+      }
+    }
+    return max;
+  }
 }
