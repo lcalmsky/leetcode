79450498c92646d6102506b8f5631ecb54dd8f08
Jaime <lcalmsky@gmail.com>
2022-02-05 04:38:39 +0900

    Solve the problem: #166

--- src/main/java/io/lcalmsky/leetcode/contiguous_array/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/contiguous_array/Solution.java
@@ -3,37 +3,21 @@ package io.lcalmsky.leetcode.contiguous_array;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * <pre>
- * Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
- *
- * Example 1:
- * Input: [0,1]
- * Output: 2
- * Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
- * Example 2:
- * Input: [0,1,0]
- * Output: 2
- * Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
- * Note: The length of the given binary array will not exceed 50,000.
- * </pre>
- */
 public class Solution {
-    public int findMaxLength(int[] nums) {
 
-        Map<Integer, Integer> sumToIndex = new HashMap<>();
-        sumToIndex.put(0, -1);
-        int sum = 0;
-        int max = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i] == 1 ? 1 : -1;
-            if (sumToIndex.containsKey(sum)) {
-                max = Math.max(max, i - sumToIndex.get(sum));
-            } else {
-                sumToIndex.put(sum, i);
-            }
-        }
-        return max;
+  public int findMaxLength(int[] nums) {
+    Map<Integer, Integer> countToIndex = new HashMap<>();
+    countToIndex.put(0, -1);
+    int sum = 0;
+    int max = 0;
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i] == 1 ? 1 : -1;
+      if (countToIndex.containsKey(sum)) {
+        max = Math.max(max, i - countToIndex.get(sum));
+      } else {
+        countToIndex.put(sum, i);
+      }
     }
+    return max;
+  }
 }
