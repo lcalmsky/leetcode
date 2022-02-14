6437b90d6d9d2b8f39325d5bcf8191d289d38435
Jaime <lcalmsky@gmail.com>
2022-02-15 01:05:32 +0900

    Solve the problem: #177

--- src/main/java/io/lcalmsky/leetcode/maximum_depth_of_binary_tree/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/maximum_depth_of_binary_tree/Solution.java
@@ -3,12 +3,13 @@ package io.lcalmsky.leetcode.maximum_depth_of_binary_tree;
 import io.lcalmsky.leetcode.TreeNode;
 
 public class Solution {
-    public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
 
-        int leftDepth = maxDepth(root.left);
-        int rightDepth = maxDepth(root.right);
-
-        return Math.max(leftDepth, rightDepth) + 1;
+  public int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
     }
+    int leftDepth = maxDepth(root.left);
+    int rightDepth = maxDepth(root.right);
+    return Math.max(leftDepth, rightDepth) + 1;
+  }
 }
