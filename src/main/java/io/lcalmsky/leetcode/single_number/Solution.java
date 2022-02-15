73d91027aee44ba25b028c03465c46a01c00674a
Jaime <lcalmsky@gmail.com>
2022-02-15 20:49:55 +0900

    Solve the problem: #179

--- src/main/java/io/lcalmsky/leetcode/single_number/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/single_number/Solution.java
@@ -1,10 +1,13 @@
 package io.lcalmsky.leetcode.single_number;
 
 public class Solution {
-    public int singleNumber(int[] nums) {
-        int x = 0;
-        for (int num : nums) x ^= num;
-        return x;
-        // return Arrays.stream(nums).reduce(0, (a, b) -> a ^ b);
+
+  public int singleNumber(int[] nums) {
+    int x = 0;
+    for (int num : nums) {
+      x ^= num;
+      System.out.println(x);
     }
+    return x;
+  }
 }
