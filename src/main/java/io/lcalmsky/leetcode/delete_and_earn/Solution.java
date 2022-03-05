2da206974fe1ddbc8b06b3602d62b4843dcf8b64
Jaime <lcalmsky@gmail.com>
2022-03-06 01:51:18 +0900

    Solve the problem: #206

--- src/main/java/io/lcalmsky/leetcode/delete_and_earn/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/delete_and_earn/Solution.java
@@ -1,21 +1,26 @@
 package io.lcalmsky.leetcode.delete_and_earn;
 
 public class Solution {
-    public int deleteAndEarn(int[] nums) {
-        int max = 0;
-        for (int num : nums) max = Math.max(max, num);
-        int[] points = new int[max + 1];
-        for (int num : nums) points[num] += num;
-        return rob(points);
-    }
 
-    private int rob(int[] points) {
-        long rob = 0, notRob = 0;
-        for (int point : points) {
-            long currRob = notRob + point;
-            notRob = Math.max(notRob, rob);
-            rob = currRob;
-        }
-        return (int) Math.max(rob, notRob);
+  public int deleteAndEarn(int[] nums) {
+    int max = 0;
+    for (int num : nums) {
+      max = Math.max(max, num);
     }
+    int[] points = new int[max + 1];
+    for (int num : nums) {
+      points[num] += num;
+    }
+    return rob(points);
+  }
+
+  private int rob(int[] points) {
+    long rob = 0, notRob = 0;
+    for (int point : points) {
+      long currRob = notRob + point;
+      notRob = Math.max(notRob, rob);
+      rob = currRob;
+    }
+    return (int) Math.max(rob, notRob);
+  }
 }
--- src/test/java/io/lcalmsky/leetcode/delete_and_earn/SolutionTest.java
+++ src/test/java/io/lcalmsky/leetcode/delete_and_earn/SolutionTest.java
@@ -1,26 +1,25 @@
 package io.lcalmsky.leetcode.delete_and_earn;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 class SolutionTest {
-    @Test
-    public void givenIntegers_whenPickAndDelete_thenReturnMaximumValue() {
-        assertAll(
-                () -> test(new int[]{3, 4, 2}, 6),
-                () -> test(new int[]{2, 2, 3, 3, 3, 4}, 9)
-        );
-    }
 
-    private void test(int[] given, int expected) {
-        // when
-        Solution solution = new Solution();
-        int actual = solution.deleteAndEarn(given);
+  @Test
+  void givenIntegers_whenPickAndDelete_thenReturnMaximumValue() {
+    assertAll(
+        () -> test(new int[]{3, 4, 2}, 6),
+        () -> test(new int[]{2, 2, 3, 3, 3, 4}, 9)
+    );
+  }
 
-        // then
-        assertEquals(expected, actual);
-    }
-
+  private void test(int[] given, int expected) {
+    // when
+    Solution solution = new Solution();
+    int actual = solution.deleteAndEarn(given);
+    // then
+    assertEquals(expected, actual);
+  }
 }
