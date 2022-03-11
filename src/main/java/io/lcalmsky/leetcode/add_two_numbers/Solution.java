0b0d7fec2bdcc67892e51995333a611a69540198
Jaime <lcalmsky@gmail.com>
2022-03-11 09:16:06 +0900

    Solve the problem: #214

--- src/main/java/io/lcalmsky/leetcode/add_two_numbers/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/add_two_numbers/Solution.java
@@ -2,32 +2,30 @@ package io.lcalmsky.leetcode.add_two_numbers;
 
 import io.lcalmsky.leetcode.ListNode;
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode(int x) { val = x; }
- * }
- */
 public class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode p1 = l1, p2 = l2, head = null, prev = null, curr;
-        short c = 0;
-        while (p1 != null || p2 != null) {
-            c += p1 != null ? p1.val : 0;
-            c += p2 != null ? p2.val : 0;
-            curr = new ListNode(c % 10);
-            c /= 10;
-            if (head == null) head = curr;
-            if (prev != null) prev.next = curr;
-            prev = curr;
-            if (p1 != null) p1 = p1.next;
-            if (p2 != null) p2 = p2.next;
-            if (c > 0) curr.next = new ListNode(c);
-        }
 
-        return head;
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode dummyHead = new ListNode(0);
+    ListNode p = l1, q = l2, current = dummyHead;
+    int carry = 0;
+    while (p != null || q != null) {
+      int x = (p != null) ? p.val : 0;
+      int y = (q != null) ? q.val : 0;
+      int sum = carry + x + y;
+      carry = sum / 10;
+      current.next = new ListNode(sum % 10);
+      current = current.next;
+      if (p != null) {
+        p = p.next;
+      }
+      if (q != null) {
+        q = q.next;
+      }
     }
+    if (carry > 0) {
+      current.next = new ListNode(carry);
+    }
+    return dummyHead.next;
+  }
 }
 
