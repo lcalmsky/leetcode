7bcec461eab83d7abd99f36d2d0d8d3d7d22185a
이정민(Jungmin Lee)/Payment개발팀/11ST <jaime.lee@sk.com>
2022-03-17 09:31:29 +0900

    Solve the problem: #224

--- src/test/java/io/lcalmsky/leetcode/score_of_parentheses/SolutionTest.java
+++ src/test/java/io/lcalmsky/leetcode/score_of_parentheses/SolutionTest.java
@@ -1,27 +1,28 @@
 package io.lcalmsky.leetcode.score_of_parentheses;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 class SolutionTest {
-    @Test
-    public void givenParentheses_whenCompute_thenCorrect() {
-        assertAll(
-                () -> test("()", 1),
-                () -> test("(())", 2),
-                () -> test("()()", 2),
-                () -> test("(()(()))", 6)
-        );
-    }
 
-    private void test(String given, int expected) {
-        // when
-        Solution solution = new Solution();
-        int actual = solution.scoreOfParentheses(given);
+  @Test
+  void givenParentheses_whenCompute_thenCorrect() {
+    assertAll(
+        () -> test("()", 1),
+        () -> test("(())", 2),
+        () -> test("()()", 2),
+        () -> test("(()(()))", 6)
+    );
+  }
 
-        // then
-        assertEquals(expected, actual);
-    }
+  private void test(String given, int expected) {
+    // when
+    Solution solution = new Solution();
+    int actual = solution.scoreOfParentheses(given);
+
+    // then
+    assertEquals(expected, actual);
+  }
 }
