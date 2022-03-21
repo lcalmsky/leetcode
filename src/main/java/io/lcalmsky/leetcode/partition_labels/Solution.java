8f7896342879d319a4ea19dbe3f4cacbf4e7619f
Jaime <lcalmsky@gmail.com>
2022-03-22 03:33:47 +0900

    Solve the problem: #228

--- src/main/java/io/lcalmsky/leetcode/partition_labels/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/partition_labels/Solution.java
@@ -4,25 +4,28 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Solution {
-    public List<Integer> partitionLabels(String S) {
-        List<Integer> result = new LinkedList<>();
-        char[] array = S.toCharArray();
-        int length = array.length;
-        int[] lastIndices = new int[26];
-        for (int i = 0; i < length; i++) lastIndices[array[i] - 'a'] = i;
-        int max = lastIndices[array[0] - 'a'];
-        int s = 0;
-        for (int i = 0; i < length; i++) {
-            if (i == max) {
-                result.add(max - s + 1);
-                s = max + 1;
-                if (i + 1 < length) {
-                    max = lastIndices[array[i + 1] - 'a'];
-                }
-            } else {
-                max = Math.max(lastIndices[array[i] - 'a'], max);
-            }
-        }
-        return result;
+
+  public List<Integer> partitionLabels(String S) {
+    List<Integer> result = new LinkedList<>();
+    char[] array = S.toCharArray();
+    int length = array.length;
+    int[] lastIndices = new int[26];
+    for (int i = 0; i < length; i++) {
+      lastIndices[array[i] - 'a'] = i;
     }
+    int max = lastIndices[array[0] - 'a'];
+    int s = 0;
+    for (int i = 0; i < length; i++) {
+      if (i == max) {
+        result.add(max - s + 1);
+        s = max + 1;
+        if (i + 1 < length) {
+          max = lastIndices[array[i + 1] - 'a'];
+        }
+      } else {
+        max = Math.max(lastIndices[array[i] - 'a'], max);
+      }
+    }
+    return result;
+  }
 }
