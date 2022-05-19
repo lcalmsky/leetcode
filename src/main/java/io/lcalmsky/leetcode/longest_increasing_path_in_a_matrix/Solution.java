0402e945b8af39835ec502c308459aa7a7913367
Jaime (Jungmin Lee) <lcalmsky@gmail.com>
2022-05-20 02:46:54 +0900

    Solve the problem: resolve #302 (#303)

--- src/main/java/io/lcalmsky/leetcode/longest_increasing_path_in_a_matrix/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/longest_increasing_path_in_a_matrix/Solution.java
@@ -1,43 +1,42 @@
 package io.lcalmsky.leetcode.longest_increasing_path_in_a_matrix;
 
 public class Solution {
-    public int longestIncreasingPath(int[][] matrix) {
-        int result = 0;
-        if (matrix == null || matrix.length == 0) return result;
-        int m = matrix.length;
-        int n = matrix[0].length;
 
-        int[][] mem = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int t = helper(matrix, mem, i, j);
-                result = Math.max(result, t);
-            }
-        }
+  private static final int[][] DIRECTIONS = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-        return result;
+  public int longestIncreasingPath(int[][] matrix) {
+    if (matrix == null || matrix.length == 0) {
+      return 0;
     }
-
-    private int helper(int[][] matrix, int[][] mem, int i, int j) {
-        if (mem[i][j] > 0) {
-            return mem[i][j];
-        }
-
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, 1, 0, -1};
-
-        for (int k = 0; k < 4; k++) {
-            int x = i + dx[k];
-            int y = j + dy[k];
-
-            if (x >= 0 && y >= 0
-                    && x < matrix.length
-                    && y < matrix[0].length
-                    && matrix[x][y] > matrix[i][j]) {
-                mem[i][j] = Math.max(mem[i][j], helper(matrix, mem, x, y));
-            }
-        }
-
-        return ++mem[i][j];
+    int result = 1;
+    int m = matrix.length;
+    int n = matrix[0].length;
+    int[][] dp = new int[m][n];
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        result = Math.max(result, dfs(matrix, dp, i, j));
+      }
     }
+    return result;
+  }
+
+  private int dfs(int[][] matrix, int[][] dp, int i, int j) {
+    if (dp[i][j] > 0) {
+      return dp[i][j];
+    }
+    int max = 1;
+    for (int[] direction : DIRECTIONS) {
+      int x = i + direction[0];
+      int y = j + direction[1];
+      if (!isInBoundary(matrix, x, y) || matrix[x][y] <= matrix[i][j]) {
+        continue;
+      }
+      max = Math.max(max, 1 + dfs(matrix, dp, x, y));
+    }
+    return dp[i][j] = max;
+  }
+
+  private boolean isInBoundary(int[][] matrix, int x, int y) {
+    return x >= 0 && y >= 0 && x < matrix.length && y < matrix[0].length;
+  }
 }
