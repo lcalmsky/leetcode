0edd53c5eea5f0ca5458fb9136bef9f7ca690940
Jaime <lcalmsky@gmail.com>
2022-06-10 02:44:57 +0900

    Solve the problem: #310

--- src/main/java/io/lcalmsky/leetcode/two_sum_ii_input_array_is_sorted/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/two_sum_ii_input_array_is_sorted/Solution.java
@@ -1,18 +1,20 @@
 package io.lcalmsky.leetcode.two_sum_ii_input_array_is_sorted;
 
 public class Solution {
-    public int[] twoSum(int[] numbers, int target) {
 
-        int left = 0;
-        int right = numbers.length - 1;
-
-        while (left < right) {
-            int x = numbers[left] + numbers[right];
-            if (x < target) ++left;
-            else if (x > target) right--;
-            else return new int[]{left + 1, right + 1};
-        }
-
-        return null;
+  public int[] twoSum(int[] numbers, int target) {
+    int left = 0;
+    int right = numbers.length - 1;
+    while (left < right) {
+      int sum = numbers[left] + numbers[right];
+      if (sum < target) {
+        left++;
+      } else if (sum > target) {
+        right--;
+      } else {
+        return new int[]{left + 1, right + 1};
+      }
     }
+    throw new IllegalStateException("cannot reach here");
+  }
 }
