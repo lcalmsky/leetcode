435b80fd86afc7ee49f511021cc04ae73fda2bbb
Jaime <lcalmsky@gmail.com>
2022-06-23 02:40:31 +0900

    Solve the problem: #315

--- src/main/java/io/lcalmsky/leetcode/kth_largest_element_in_an_array/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/kth_largest_element_in_an_array/Solution.java
@@ -1,10 +1,20 @@
 package io.lcalmsky.leetcode.kth_largest_element_in_an_array;
 
-import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length - k];
+
+  public int findKthLargest(int[] nums, int k) {
+    PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+    for (int num : nums) {
+      priorityQueue.offer(num);
     }
+    while (priorityQueue.size() > k) {
+      priorityQueue.poll();
+    }
+    if (priorityQueue.isEmpty()) {
+      throw new IllegalStateException();
+    }
+    return priorityQueue.poll();
+  }
 }
---
+++ src/test/java/io/lcalmsky/leetcode/kth_largest_element_in_an_array/SolutionTest.java
@@ -0,0 +1,26 @@
+package io.lcalmsky.leetcode.kth_largest_element_in_an_array;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+  @Test
+  public void givenNumbers_whenFindKthNumber_thenCorrect() {
+    assertAll(
+        () -> test(new int[]{3, 2, 1, 5, 6, 4}, 2, 5),
+        () -> test(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4, 4)
+    );
+  }
+
+  private void test(int[] nums, int k, int expected) {
+    // when
+    Solution kthLargestElementInAnArray = new Solution();
+    int actual = kthLargestElementInAnArray.findKthLargest(nums, k);
+
+    // then
+    assertEquals(expected, actual);
+  }
+}
