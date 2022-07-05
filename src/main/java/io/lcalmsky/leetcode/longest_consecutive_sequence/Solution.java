8256f1470ccc714c890338c5980b19c5ebf5f95a
Jaime <lcalmsky@gmail.com>
2022-07-05 23:07:48 +0900

    Solve the problem: #324

--- src/main/java/io/lcalmsky/leetcode/longest_consecutive_sequence/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/longest_consecutive_sequence/Solution.java
@@ -1,31 +1,29 @@
 package io.lcalmsky.leetcode.longest_consecutive_sequence;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 public class Solution {
-    public int longestConsecutive(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) set.add(num);
 
-        int longestConsecutiveSequence = 0;
-        int count, down, up;
-        for (int num : nums) {
-            count = 1;
-            down = num - 1;
-            while (set.contains(down)) {
-                set.remove(down);
-                down--;
-                count++;
-            }
-            up = num + 1;
-            while (set.contains(up)) {
-                set.remove(up);
-                up++;
-                count++;
-            }
-            longestConsecutiveSequence = Math.max(longestConsecutiveSequence, count);
-        }
-        return longestConsecutiveSequence;
+  public int longestConsecutive(int[] nums) {
+    if (nums.length == 0) {
+      return 0;
     }
+    Arrays.sort(nums);
+    int longest = 1;
+    int current = 1;
+    for (int i = 1; i < nums.length; i++) {
+      int currentNumber = nums[i];
+      int previousNumber = nums[i - 1];
+      if (currentNumber == previousNumber) {
+        continue;
+      }
+      if (currentNumber == previousNumber + 1) {
+        current += 1;
+      } else {
+        longest = Math.max(longest, current);
+        current = 1;
+      }
+    }
+    return Math.max(longest, current);
+  }
 }
