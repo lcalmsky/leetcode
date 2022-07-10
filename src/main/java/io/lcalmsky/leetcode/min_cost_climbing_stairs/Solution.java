5fa0d37af04e78e6a67bc7b9b26e2dec13507f99
Jaime <lcalmsky@gmail.com>
2022-07-10 22:03:53 +0900

    Solve the problem: #326

--- src/main/java/io/lcalmsky/leetcode/min_cost_climbing_stairs/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/min_cost_climbing_stairs/Solution.java
@@ -1,15 +1,16 @@
 package io.lcalmsky.leetcode.min_cost_climbing_stairs;
 
 public class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-        int first = cost[0];
-        int second = cost[1];
-        for (int i = 2; i < n; i++) {
-            int current = cost[i] + Math.min(first, second);
-            first = second;
-            second = current;
-        }
-        return Math.min(first, second);
+
+  public int minCostClimbingStairs(int[] cost) {
+    int n = cost.length;
+    int first = cost[0];
+    int second = cost[1];
+    for (int i = 2; i < n; i++) {
+      int current = cost[i] + Math.min(first, second);
+      first = second;
+      second = current;
     }
+    return Math.min(first, second);
+  }
 }
