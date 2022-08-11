f597da02f715df6dcf40901b67564060a59047c7
Jaime <lcalmsky@gmail.com>
2022-08-11 17:11:50 +0900

    Solve the problem: #342

--- src/main/java/io/lcalmsky/leetcode/validate_binary_search_tree/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/validate_binary_search_tree/Solution.java
@@ -3,15 +3,18 @@ package io.lcalmsky.leetcode.validate_binary_search_tree;
 import io.lcalmsky.leetcode.TreeNode;
 
 public class Solution {
-    public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+
+  public boolean isValidBST(TreeNode root) {
+    return isValidBST(root, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+  }
+
+  private boolean isValidBST(TreeNode node, double min, double max) {
+    if (node == null) {
+      return true;
     }
-
-    private boolean isValidBST(TreeNode node, double min, double max) {
-        if (node == null) return true;
-
-        if (node.val <= min || node.val >= max) return false;
-
-        return isValidBST(node.left, min, node.val) && isValidBST(node.right, node.val, max);
+    if (node.val <= min || node.val >= max) {
+      return false;
     }
+    return isValidBST(node.left, min, node.val) && isValidBST(node.right, node.val, max);
+  }
 }
