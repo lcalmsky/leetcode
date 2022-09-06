164dd495a549a0230688191f452e3f42a48d690e
Jaime (Jungmin Lee) <lcalmsky@gmail.com>
2022-09-07 06:25:18 +0900

    Solve the problem: resolve #381 (#382)

--- src/test/java/io/lcalmsky/leetcode/binary_tree_pruning/SolutionTest.java
+++ src/test/java/io/lcalmsky/leetcode/binary_tree_pruning/SolutionTest.java
@@ -1,26 +1,27 @@
 package io.lcalmsky.leetcode.binary_tree_pruning;
 
-import io.lcalmsky.leetcode.TreeNode;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import io.lcalmsky.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
 class SolutionTest {
-    @Test
-    public void givenTreeNode_whenPrune_thenCorrect() {
-        assertAll(
-                () -> test(TreeNode.of(1, null, 0, 0, 1), TreeNode.of(1, null, 0, null, 1)),
-                () -> test(TreeNode.of(1, 0, 1, 0, 0, 0, 1), TreeNode.of(1, null, 1, null, 1))
-        );
-    }
 
-    private void test(TreeNode given, TreeNode expected) {
-        // when
-        Solution solution = new Solution();
-        TreeNode actual = solution.pruneTree(given);
+  @Test
+  public void givenTreeNode_whenPrune_thenCorrect() {
+    assertAll(
+        () -> test(TreeNode.of(1, null, 0, 0, 1), TreeNode.of(1, null, 0, null, 1)),
+        () -> test(TreeNode.of(1, 0, 1, 0, 0, 0, 1), TreeNode.of(1, null, 1, null, 1))
+    );
+  }
 
-        // then
-        assertEquals(expected, actual);
-    }
+  private void test(TreeNode given, TreeNode expected) {
+    // when
+    Solution solution = new Solution();
+    TreeNode actual = solution.pruneTree(given);
+
+    // then
+    assertEquals(expected, actual);
+  }
 }
