35422e4f251493ab5e241f69c553a7d4f14a49f5
Jaime (Jungmin Lee) <lcalmsky@gmail.com>
2023-06-28 01:42:42 +0900

    feat: solve the problem (resolve #407) (#408)

--- src/main/java/io/lcalmsky/leetcode/search_a_2d_matrix_ii/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/search_a_2d_matrix_ii/Solution.java
@@ -3,18 +3,17 @@ package io.lcalmsky.leetcode.search_a_2d_matrix_ii;
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0) return false;
-        int m = matrix.length - 1;
         int n = matrix[0].length - 1;
-
-        int i = m, j = 0;
+        int i = matrix.length - 1, j = 0;
         while (i >= 0 && j <= n) {
             if (target < matrix[i][j]) {
                 i--;
             } else if (target > matrix[i][j]) {
                 j++;
-            } else return true;
+            } else {
+                return true;
+            }
         }
-
         return false;
     }
 }
