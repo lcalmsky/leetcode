c88803ec1b3d020a82cca651262e319c2a5f2304
Jaime (Jungmin Lee) <lcalmsky@gmail.com>
2023-06-27 22:22:52 +0900

    feat: solve the problem (resolve #404) (#405)

--- src/test/java/io/lcalmsky/leetcode/kth_smallest_element_in_a_bst/SolutionTest.java
+++ src/test/java/io/lcalmsky/leetcode/kth_smallest_element_in_a_bst/SolutionTest.java
@@ -6,20 +6,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class KthSmallestElementInABstTests {
+class SolutionTest {
     @Test
-    public void givenTreeNode_whenFindKthSmallestElement_thenCorrect() {
+    void testAll() {
         assertAll(
                 () -> test(TreeNode.of(3, 1, 4, null, 2), 1, 1),
                 () -> test(TreeNode.of(5, 3, 6, 2, 4, null, null, 1), 3, 3)
         );
     }
 
-    private void test(TreeNode given, int k, int expected) {
+    private void test(TreeNode root, int k, int expected) {
         // when
-        Solution kthSmallestElementInABst = new Solution();
-        int actual = kthSmallestElementInABst.kthSmallest(given, k);
-
+        Solution solution = new Solution();
+        int actual = solution.kthSmallest(root, k);
         // then
         assertEquals(expected, actual);
     }
