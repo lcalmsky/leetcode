04948fede0a21a39022df085547246ef05caa8db
Jaime <lcalmsky@gmail.com>
2023-06-28 01:44:04 +0900

    Revert "feat: update the solution (#406)"
    This reverts commit bd47bbe54e2e6809b494f64c35af426c914bd9d7.

--- src/main/java/io/lcalmsky/leetcode/search_a_2d_matrix/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/search_a_2d_matrix/Solution.java
@@ -3,16 +3,23 @@ package io.lcalmsky.leetcode.search_a_2d_matrix;
 public class Solution {
 
   public boolean searchMatrix(int[][] matrix, int target) {
-    int n = matrix.length, m = matrix[0].length;
-    int i = 0, j = m - 1;
-    while (i < n && j >= 0) {
-      if (matrix[i][j] == target) {
-        return true;
-      }
-      if (matrix[i][j] < target) {
-        i++;
-      } else if (matrix[i][j] > target) {
-        j--;
+    int m = matrix.length;
+    int n = matrix[0].length;
+    for (int[] row : matrix) {
+      if (target <= row[n - 1]) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+          int mid = low + (high - low) / 2;
+          if (target == row[mid]) {
+            return true;
+          }
+          if (target < row[mid]) {
+            high = mid - 1;
+          } else {
+            low = mid + 1;
+          }
+        }
       }
     }
     return false;
--- src/test/java/io/lcalmsky/leetcode/search_a_2d_matrix/SolutionTest.java
+++ src/test/java/io/lcalmsky/leetcode/search_a_2d_matrix/SolutionTest.java
@@ -1,27 +1,26 @@
 package io.lcalmsky.leetcode.search_a_2d_matrix;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 class SolutionTest {
 
-    @Test
-    void givenMatrix_whenSearchesTarget_thenReturnsWhetherExists() {
-        assertAll(
-                () -> test(new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 13, true),
-                () -> test(new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 5, true),
-                () -> test(new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 20, false)
-        );
-    }
+  @Test
+  void givenMatrix_whenSearchesTarget_thenReturnsWhetherExists() {
+    assertAll(
+        () -> test(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 3, true),
+        () -> test(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 13, false)
+    );
+  }
 
-    private void test(int[][] givenArray, int givenTarget, boolean expected) {
-        // when
-        Solution searchA2dMatrix = new Solution();
-        boolean actual = searchA2dMatrix.searchMatrix(givenArray, givenTarget);
+  private void test(int[][] givenArray, int givenTarget, boolean expected) {
+    // when
+    Solution searchA2dMatrix = new Solution();
+    boolean actual = searchA2dMatrix.searchMatrix(givenArray, givenTarget);
 
-        // then
-        assertEquals(expected, actual);
-    }
+    // then
+    assertEquals(expected, actual);
+  }
 }
