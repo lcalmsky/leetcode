f1e83c9ef9674a182136bb37914ffd3d1b92cbf1
Jaime <lcalmsky@gmail.com>
2023-06-29 02:29:36 +0900

    feat: update solution

--- src/main/java/io/lcalmsky/leetcode/swap_node_in_pairs/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/swap_node_in_pairs/Solution.java
@@ -4,23 +4,20 @@ import io.lcalmsky.leetcode.ListNode;
 
 public class Solution {
 
-  public ListNode swapPairs(ListNode head) {
-    if (head == null || head.next == null) {
-      return head;
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode result = new ListNode(0);
+        result.next = head;
+        ListNode current = result;
+        while (current.next != null && current.next.next != null) {
+            ListNode temp = current.next;
+            current.next = current.next.next;
+            temp.next = current.next.next;
+            current.next.next = temp;
+            current = current.next.next;
+        }
+        return result.next;
     }
-    ListNode result = new ListNode(0);
-    result.next = head;
-    ListNode current = result;
-    ListNode slow;
-    ListNode fast;
-    while (current.next != null && current.next.next != null) {
-      slow = current;
-      current = current.next;
-      slow.next = current.next;
-      fast = current.next.next;
-      current.next.next = current;
-      current.next = fast;
-    }
-    return result.next;
-  }
 }
