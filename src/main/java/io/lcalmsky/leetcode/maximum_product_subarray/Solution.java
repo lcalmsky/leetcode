b853081b0f01c43b240093dbf030de97a23dfc8f
Jaime (Jungmin Lee) <lcalmsky@gmail.com>
2023-07-01 01:20:48 +0900

    feat: solve the problem (resolve #418) (#419)

--- src/main/java/io/lcalmsky/leetcode/maximum_product_subarray/Solution.java
+++ src/main/java/io/lcalmsky/leetcode/maximum_product_subarray/Solution.java
@@ -2,18 +2,20 @@ package io.lcalmsky.leetcode.maximum_product_subarray;
 
 public class Solution {
     public int maxProduct(int[] nums) {
-        int[] max = new int[nums.length];
-        int[] min = new int[nums.length];
+        int length = nums.length;
+        int[] max = new int[length];
+        int[] min = new int[length];
         max[0] = min[0] = nums[0];
         int result = nums[0];
 
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                max[i] = Math.max(nums[i], max[i - 1] * nums[i]);
-                min[i] = Math.min(nums[i], min[i - 1] * nums[i]);
+        for (int i = 1; i < length; i++) {
+            int num = nums[i];
+            if (num > 0) {
+                max[i] = Math.max(num, max[i - 1] * num);
+                min[i] = Math.min(num, min[i - 1] * num);
             } else {
-                max[i] = Math.max(nums[i], min[i - 1] * nums[i]);
-                min[i] = Math.min(nums[i], max[i - 1] * nums[i]);
+                max[i] = Math.max(num, min[i - 1] * num);
+                min[i] = Math.min(num, max[i - 1] * num);
             }
             result = Math.max(result, max[i]);
         }
