9e2a8b20af1b37d2c77f9a4e18c7647ca41f5c2d
Jaime <lcalmsky@gmail.com>
2023-07-15 00:33:22 +0900

    feat: add solution

---
+++ src/main/java/io/lcalmsky/leetcode/kth_smallest_element_in_a_bst/Solution2.java
@@ -0,0 +1,27 @@
+package io.lcalmsky.leetcode.kth_smallest_element_in_a_bst;
+
+import io.lcalmsky.leetcode.TreeNode;
+
+public class Solution2 {
+
+    private int count = 0;
+    private int result = -1;
+
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root, k);
+        return result;
+    }
+
+    private void inorder(TreeNode root, int k) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, k);
+        count++;
+        if (count == k) {
+            result = root.val;
+            return;
+        }
+        inorder(root.right, k);
+    }
+}
