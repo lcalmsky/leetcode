acc44d227ef53755bcf9d754607d47698eb6c9b9
Jaime <lcalmsky@gmail.com>
2023-07-26 22:40:50 +0900

    feat: solve the problem (resolve #457)

---
+++ src/main/java/io/lcalmsky/leetcode/kids_with_the_greatest_number_of_candies/Solution.java
@@ -0,0 +1,18 @@
+package io.lcalmsky.leetcode.kids_with_the_greatest_number_of_candies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        for (int candy : candies) {
+            max = Math.max(max, candy);
+        }
+        List<Boolean> result = new ArrayList<>();
+        for (int candy : candies) {
+            result.add(candy + extraCandies >= max);
+        }
+        return result;
+    }
+}
